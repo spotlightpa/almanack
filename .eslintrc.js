db2f8a87ac79d376e9c215140d9ce5330b02773b
Carl Johnson <me@carlmjohnson.net>
2019-11-22 09:22:54 -0500

    ESLint: No surprises in deployment!

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-console": "warn",
+    "no-debugger": "warn"
   },
   parserOptions: {
     parser: "babel-eslint"
