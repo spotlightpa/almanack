ca32ddbd1b75854b804221e291225f25dc32b850
Carl Johnson <me@carlmjohnson.net>
2019-11-22 22:03:07 -0500

    ESLint: Use settings from poor-richard

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,70 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": "warn",
-    "no-debugger": "warn"
   },
   parserOptions: {
     parser: "babel-eslint"
+  },
+  extends: [
+    "plugin:vue/essential",
+    "plugin:prettier/recommended",
+    "eslint:recommended",
+    "prettier",
+    "prettier/vue"
+  ],
+  plugins: ["prettier", "vue"],
+  // add your custom rules here
+  rules: {
+    "no-console": "warn",
+    "vue/attribute-hyphenation": ["warn", "always"],
+    "vue/attributes-order": [
+      "warn",
+      {
+        order: [
+          "DEFINITION",
+          "LIST_RENDERING",
+          "CONDITIONALS",
+          "RENDER_MODIFIERS",
+          "GLOBAL",
+          "UNIQUE",
+          "TWO_WAY_BINDING",
+          "OTHER_DIRECTIVES",
+          "OTHER_ATTR",
+          "EVENTS",
+          "CONTENT"
+        ]
+      }
+    ],
+    "vue/mustache-interpolation-spacing": ["warn", "always"],
+    "vue/name-property-casing": ["warn", "PascalCase"],
+    "vue/order-in-components": [
+      "warn",
+      {
+        order: [
+          "el",
+          "name",
+          "parent",
+          "functional",
+          ["delimiters", "comments"],
+          ["components", "directives", "filters"],
+          "extends",
+          "mixins",
+          "inheritAttrs",
+          "model",
+          ["props", "propsData"],
+          "data",
+          "computed",
+          "watch",
+          "LIFECYCLE_HOOKS",
+          "methods",
+          ["template", "render"],
+          "renderError"
+        ]
+      }
+    ],
+    "vue/this-in-template": ["error", "never"],
+    "vue/v-bind-style": ["warn", "shorthand"]
   }
 };
