78623360e7c663f52e3cb6b6b3bfa79240004a81
Carl Johnson <me@carlmjohnson.net>
2019-11-22 22:02:42 -0500

    Yarn: Add purgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+
+let plugins = [require("autoprefixer")];
+if (process.env.NODE_ENV === "production") {
+  plugins.push(
+    purgecss({
+      content: ["./**/*.html", "./**/*.vue"]
+    })
+  );
+}
+
 module.exports = {
-  plugins: {
-    autoprefixer: {}
-  }
+  plugins
 };
