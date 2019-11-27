a8d9245faac9f29e25a7050f875159852b5fd72d
Carl Johnson <me@carlmjohnson.net>
2019-11-27 13:42:26 -0500

    Vue: Add error route

---
+++ src/components/ViewError.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="section container">
+    <div class="content is-large">
+      <h2>Error</h2>
+      <p class=""><router-link :to="{ name: 'home' }">Go home</router-link>?</p>
+    </div>
+  </div>
+</template>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
-import ViewLogin from "./components/ViewLogin.vue";
 import ViewArticleList from "./components/ViewArticleList.vue";
+import ViewError from "./components/ViewError.vue";
+import ViewLogin from "./components/ViewLogin.vue";
 
 import { authGuard } from "./plugins/auth.js";
 
@@ -11,7 +12,7 @@ export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
-    { path: "/", redirect: { name: "home" } },
+    { path: "/", name: "home", redirect: { name: "articles" } },
     {
       path: "/login",
       name: "login",
@@ -19,9 +20,14 @@ export default new Router({
     },
     {
       path: "/articles",
-      name: "home",
+      name: "articles",
       component: ViewArticleList,
       beforeEnter: authGuard
+    },
+    {
+      path: "/*",
+      name: "error",
+      component: ViewError
     }
   ]
 });
