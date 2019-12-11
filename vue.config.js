9cab6c9e3811a6b7c05c81558c13e3a8d0e9045e
Carl Johnson <me@carlmjohnson.net>
2019-12-11 12:50:49 -0500

    Vue config: Autolint on save

--- vue.config.js
+++ vue.config.js
@@ -10,4 +10,12 @@ module.exports = {
       },
     },
   },
+  chainWebpack: config => {
+    config.module
+      .rule("eslint")
+      .use("eslint-loader")
+      .options({
+        fix: true,
+      });
+  },
 };
