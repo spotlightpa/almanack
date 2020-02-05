7b97e42785f87c0f89e02d84277df92d8ac8d1fd
Carl Johnson <me@carlmjohnson.net>
2020-02-05 14:00:30 -0500

    Add Slack notifications

--- cmd/identity-signup/identity-signup-main.go
+++ cmd/identity-signup/identity-signup-main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
@@ -10,11 +11,18 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 
 	"github.com/spotlightpa/almanack/internal/netlifyid"
+	"github.com/spotlightpa/almanack/internal/slack"
 )
 
 func main() {
 	lambda.Start(whitelistEmails)
 }
+
+var (
+	whitelisted_domains = os.Getenv("ALMANACK_WHITELIST_DOMAINS")
+	slackHookURL        = os.Getenv("ALMANACK_SLACK_HOOK_URL")
+	logger              = log.New(os.Stdout, "identity-signup ", log.LstdFlags)
+)
 
 func whitelistEmails(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
@@ -35,11 +43,11 @@ func whitelistEmails(request events.APIGatewayProxyRequest) (events.APIGatewayPr
 		data.User.AppMetadata.Roles = append(data.User.AppMetadata.Roles,
 			"arc user")
 	}
-	suffixes := strings.FieldsFunc(os.Getenv("ALMANACK_WHITELIST_DOMAINS"),
+	suffixes := strings.FieldsFunc(whitelisted_domains,
 		func(r rune) bool { return r == ',' || r == ' ' })
 	for _, suffix := range suffixes {
 		if strings.HasSuffix(data.User.Email, suffix) {
-			fmt.Printf("%s has domain %s", data.User.Email, suffix)
+			logger.Printf("%s has domain %s", data.User.Email, suffix)
 			data.User.AppMetadata.Roles = append(data.User.AppMetadata.Roles, "editor")
 			break
 		}
@@ -49,6 +57,24 @@ func whitelistEmails(request events.APIGatewayProxyRequest) (events.APIGatewayPr
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
 	}
+	msg := fmt.Sprintf("%s <%s> with %d role(s)",
+		data.User.UserMetadata.FullName,
+		data.User.Email,
+		len(data.User.AppMetadata.Roles))
+	slack.New(slackHookURL, logger).Post(
+		slack.Message{
+			Attachments: []slack.Attachment{
+				{
+					Title: "New Almanack Registration",
+					Text:  msg,
+					Color: "#78bc20",
+					Fields: []slack.Field{
+						{
+							Title: "Roles",
+							Value: strings.Join(data.User.AppMetadata.Roles, ", "),
+							Short: true,
+						}}}}},
+	)
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       string(body),
