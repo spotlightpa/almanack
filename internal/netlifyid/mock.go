b4d4d0c55a7802a0fd65376c6f78ac8fcd3a5b88
Carl Johnson <me@carlmjohnson.net>
2020-02-12 12:21:38 -0500

    Mock auth: Ensure that auth header is present

--- internal/netlifyid/mock.go
+++ internal/netlifyid/mock.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/spotlightpa/almanack/pkg/almanack"
+	"github.com/spotlightpa/almanack/pkg/errutil"
 )
 
 type MockAuthService struct{ almanack.Logger }
@@ -17,5 +18,8 @@ func (mas MockAuthService) AddToRequest(r *http.Request) (*http.Request, error) 
 
 func (mas MockAuthService) HasRole(r *http.Request, role string) error {
 	mas.Logger.Printf("mock auth checking for role %q", role)
+	if r.Header.Get("Authorization") == "" {
+		return errutil.Unauthorized
+	}
 	return nil
 }
