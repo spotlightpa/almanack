cc7b8ff10a9f4bb123fe66ef9a2fd57ab3c35db2
Carl Johnson <me@carlmjohnson.net>
2020-02-18 12:58:55 -0500

    Add footer with license disclaimer

--- src/components/TheApp.vue
+++ src/components/TheApp.vue
@@ -11,14 +11,36 @@ export default {
 </script>
 
 <template>
-  <div>
+  <div class="body">
     <PageNav id="top-nav" />
     <main class="section container">
-      <div class="columns is-centered">
+      <div class="columns is-centered is-multiline">
         <div class="column is-four-fifths">
           <router-view />
         </div>
       </div>
     </main>
+    <section class="footer is-size-4">
+      <div class="columns is-centered is-multiline">
+        <div class="column is-two-fifths has-text-centered is-size-5">
+          <p>
+            © <span v-text="new Date().getFullYear()"></span> <br />
+            Spotlight PA <br />
+            225 Market St Suite 502A <br />
+            Harrisburg PA 17101
+          </p>
+          <p>
+            <em>
+              All Spotlight PA content is provided “as-is” and its use is at the
+              sole risk and discretion of partner news organizations. To view
+              the full licensing agreement,
+              <a href="/docs/Spotlight PA Content License.docx" download
+                >click here</a
+              >.
+            </em>
+          </p>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
