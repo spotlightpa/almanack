10798e13f298e00709da3b38a7fd90e23b7a544b
Carl Johnson <me@carlmjohnson.net>
2020-02-20 20:42:24 -0500

    Rename makeService, makeClient; extract ImageUploader component

--- src/api/hooks.js
+++ src/api/hooks.js
@@ -4,7 +4,7 @@ import VueCompositionAPI from "@vue/composition-api";
 Vue.use(VueCompositionAPI);
 
 import { makeAuth } from "./auth.js";
-import { makeService } from "./service.js";
+import { makeClient } from "./service.js";
 import { listAvailable, available } from "./arc-services.js";
 import { getScheduledArticle } from "./scheduler.js";
 
@@ -17,27 +17,27 @@ export function useAuth() {
   return $auth;
 }
 
-let $service;
+let $client;
 
-export function useService() {
-  if (!$service) {
-    $service = makeService(useAuth());
+export function useClient() {
+  if (!$client) {
+    $client = makeClient(useAuth());
   }
-  return $service;
+  return $client;
 }
 
 export function useFeed() {
-  let feed = listAvailable(useService());
+  let feed = listAvailable(useClient());
   feed.initLoad();
   return feed;
 }
 
 export function getAvailableArticle() {
-  return available(useService());
+  return available(useClient());
 }
 
 export function useScheduler(id) {
-  let obj = getScheduledArticle({ service: useService(), id });
+  let obj = getScheduledArticle({ service: useClient(), id });
   obj.initLoad();
   return obj;
 }
