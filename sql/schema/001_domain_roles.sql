b89a24e72b7e01db2cf656caecac35876d47a4d7
Carl Johnson <me@carlmjohnson.net>
2020-03-03 15:40:11 -0500

    Postgres: Add database for domain roles with SQLC+Tern

---
+++ sql/schema/001_domain_roles.sql
@@ -0,0 +1,27 @@
+CREATE TABLE domain_roles (
+    id serial PRIMARY KEY,
+    domain text NOT NULL,
+    roles text[],
+    created_at timestamp WITH time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp WITH time zone NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE UNIQUE INDEX unique_domain_on_domain_roles ON domain_roles ((lower(domain)) text_ops);
+
+CREATE OR REPLACE FUNCTION update_row_updated_at_function_ ()
+    RETURNS TRIGGER
+    AS $$
+BEGIN
+    NEW.updated_at = clock_timestamp();
+    RETURN NEW;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER row_updated_at_on_domain_roles_trigger_
+    BEFORE UPDATE ON domain_roles
+    FOR EACH ROW
+    EXECUTE PROCEDURE update_row_updated_at_function_ ();
+
+---- create above / drop below ----
+DROP TABLE domain_roles;
