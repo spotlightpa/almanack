98a02fbbe245bede3c147e7a2e6e508b1ea2d63d
Carl Johnson <me@carlmjohnson.net>
2020-03-12 12:40:06 -0400

    Get save scheduled article working

--- pkg/almanack/scheduled-article.go
+++ pkg/almanack/scheduled-article.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
-	"errors"
 	"time"
 
 	"github.com/spotlightpa/almanack/internal/db"
-	"github.com/spotlightpa/almanack/pkg/errutil"
 )
 
 type ScheduledArticle struct {
@@ -16,6 +14,71 @@ type ScheduledArticle struct {
 	ScheduleFor *time.Time
 	LastArcSync time.Time
 	LastSaved   *time.Time
+}
+
+func (schArticle *ScheduledArticle) toSPLData() interface{} {
+	type splDataType struct {
+		ID               *string     `json:"internal-id"`
+		Budget           *string     `json:"budget"`
+		ImageURL         *string     `json:"image-url"`
+		ImageCaption     *string     `json:"image-caption"`
+		ImageCredit      *string     `json:"image-credit"`
+		PubDate          *time.Time  `json:"pub-date"`
+		Slug             *string     `json:"slug"`
+		Authors          *[]string   `json:"authors"`
+		Byline           *string     `json:"byline"`
+		Hed              *string     `json:"hed"`
+		Subhead          *string     `json:"subhead"`
+		Summary          *string     `json:"summary"`
+		Blurb            *string     `json:"blurb"`
+		Kicker           *string     `json:"kicker"`
+		Body             *string     `json:"body"`
+		LinkTitle        *string     `json:"link-title"`
+		SuppressFeatured *bool       `json:"suppress-featured"`
+		LastArcSync      *time.Time  `json:"last-arc-sync"`
+		LastSaved        **time.Time `json:"last-saved"`
+	}
+	return &splDataType{
+		ID:               &schArticle.ID,
+		Budget:           &schArticle.Budget,
+		ImageURL:         &schArticle.ImageURL,
+		ImageCaption:     &schArticle.ImageCaption,
+		ImageCredit:      &schArticle.ImageCredit,
+		PubDate:          &schArticle.PubDate,
+		Slug:             &schArticle.Slug,
+		Authors:          &schArticle.Authors,
+		Byline:           &schArticle.Byline,
+		Hed:              &schArticle.Hed,
+		Subhead:          &schArticle.Subhead,
+		Summary:          &schArticle.Summary,
+		Blurb:            &schArticle.Blurb,
+		Kicker:           &schArticle.Kicker,
+		Body:             &schArticle.Body,
+		LinkTitle:        &schArticle.LinkTitle,
+		SuppressFeatured: &schArticle.SuppressFeatured,
+		LastArcSync:      &schArticle.LastArcSync,
+		LastSaved:        &schArticle.LastSaved,
+	}
+}
+
+func ScheduledArticleFromDB(dbArticle db.Article) (*ScheduledArticle, error) {
+	var schArticle ScheduledArticle
+	schArticle.ArcID = dbArticle.ArcID.String
+	if dbArticle.ScheduleFor.Valid {
+		t := dbArticle.ScheduleFor.Time
+		schArticle.ScheduleFor = &t
+	}
+
+	if err := json.Unmarshal(dbArticle.SpotlightPAData, schArticle.toSPLData()); err != nil {
+		return nil, err
+	}
+
+	if schArticle.LastArcSync.IsZero() {
+		if err := schArticle.ResetArcData(dbArticle); err != nil {
+			return nil, err
+		}
+	}
+	return &schArticle, nil
 }
 
 func (schArticle *ScheduledArticle) ResetArcData(dbArticle db.Article) error {
@@ -32,58 +95,24 @@ func (schArticle *ScheduledArticle) ResetArcData(dbArticle db.Article) error {
 	return nil
 }
 
+func (schArticle *ScheduledArticle) ToDB() (*db.Article, error) {
+	var dart db.Article
+
+	dart.ArcID = nullString(schArticle.ArcID)
+	dart.SpotlightPAPath = nullString(schArticle.ContentFilepath())
+	dart.ScheduleFor = nullTime(schArticle.ScheduleFor)
+	var err error
+	if dart.SpotlightPAData, err = json.Marshal(schArticle.toSPLData()); err != nil {
+		return nil, err
+	}
+	return &dart, nil
+}
+
 type ScheduledArticleService struct {
 	ArticleService
-	DataStore
 	ContentStore
 	Logger
 	Querier db.Querier
-}
-
-func (sas ScheduledArticleService) get(articleID string) (*ScheduledArticle, error) {
-	var data ScheduledArticle
-	err := sas.DataStore.Get("almanack.scheduled-article."+articleID, &data)
-	switch {
-	case err == nil:
-		return &data, nil
-	default:
-		return nil, err
-	case errors.Is(err, errutil.NotFound):
-		return nil, nil
-	}
-}
-
-func (sas ScheduledArticleService) set(article *ScheduledArticle) error {
-	articleID := article.ArcID
-	return sas.DataStore.Set("almanack.scheduled-article."+articleID, article)
-}
-
-func (sas ScheduledArticleService) lock() (unlock func(), err error) {
-	return sas.DataStore.GetLock("almanack.scheduled-articles-lock")
-}
-
-// Get the existing list of scheduled articles
-func (sas ScheduledArticleService) listIDs() (map[string]bool, error) {
-	ids := map[string]bool{}
-	err := sas.DataStore.Get("almanack.scheduled-articles-list", &ids)
-	if errors.Is(err, errutil.NotFound) {
-		err = nil
-	}
-	prune(ids)
-	return ids, err
-}
-
-func (sas ScheduledArticleService) setIDs(ids map[string]bool) error {
-	prune(ids)
-	return sas.DataStore.Set("almanack.scheduled-articles-list", &ids)
-}
-
-func prune(ids map[string]bool) {
-	for k, v := range ids {
-		if !v {
-			delete(ids, k)
-		}
-	}
 }
 
 func (sas ScheduledArticleService) Get(ctx context.Context, articleID string) (*ScheduledArticle, error) {
@@ -97,13 +126,7 @@ func (sas ScheduledArticleService) Get(ctx context.Context, articleID string) (*
 }
 
 func (sas ScheduledArticleService) Save(ctx context.Context, article *ScheduledArticle) error {
-	// Get the lock
-	unlock, err := sas.lock()
-	if err != nil {
-		return err
-	}
-	defer unlock()
-
+	// TODO: Make less racey
 	if article.ScheduleFor != nil &&
 		article.ScheduleFor.Before(time.Now().Add(5*time.Minute)) {
 		article.ScheduleFor = nil
@@ -115,26 +138,28 @@ func (sas ScheduledArticleService) Save(ctx context.Context, article *ScheduledA
 	// Save the article
 	now := time.Now()
 	article.LastSaved = &now
-	if err := sas.set(article); err != nil {
-		return err
-	}
 
-	ids, err := sas.listIDs()
+	dart, err := article.ToDB()
 	if err != nil {
 		return err
 	}
 
-	// If the status of the article changed, update the list
-	shouldPub := article.ScheduleFor != nil
-	hasChanged := shouldPub != ids[article.ArcID]
-
-	if hasChanged {
-		ids[article.ArcID] = shouldPub
-		if err := sas.setIDs(ids); err != nil {
-			return err
-		}
+	start := time.Now()
+	*dart, err = sas.Querier.UpdateSpotlightPAArticle(ctx, db.UpdateSpotlightPAArticleParams{
+		ArcID:           dart.ArcID,
+		SpotlightPAData: dart.SpotlightPAData,
+		ScheduleFor:     dart.ScheduleFor,
+		SpotlightPAPath: dart.SpotlightPAPath,
+	})
+	sas.Logger.Printf("queried UpdateSpotlightPAArticle in %v", time.Since(start))
+	if err != nil {
+		return err
 	}
-
+	updatedArticle, err := ScheduledArticleFromDB(*dart)
+	if err != nil {
+		return err
+	}
+	*article = *updatedArticle
 	return nil
 }
 
@@ -162,47 +187,10 @@ func (sas ScheduledArticleService) PopScheduledArticles(ctx context.Context, cal
 	return nil
 }
 
-func ScheduledArticleFromDB(dbArticle db.Article) (*ScheduledArticle, error) {
-	var schArticle ScheduledArticle
-	schArticle.ArcID = dbArticle.ArcID.String
-	if dbArticle.ScheduleFor.Valid {
-		t := dbArticle.ScheduleFor.Time
-		schArticle.ScheduleFor = &t
-	}
-	spotlightPADataVal := map[string]interface{}{
-		"ID":               &schArticle.ID,
-		"Budget":           &schArticle.Budget,
-		"ImageURL":         &schArticle.ImageURL,
-		"ImageCaption":     &schArticle.ImageCaption,
-		"ImageCredit":      &schArticle.ImageCredit,
-		"PubDate":          &schArticle.PubDate,
-		"Slug":             &schArticle.Slug,
-		"Authors":          &schArticle.Authors,
-		"Byline":           &schArticle.Byline,
-		"Hed":              &schArticle.Hed,
-		"Subhead":          &schArticle.Subhead,
-		"Summary":          &schArticle.Summary,
-		"Blurb":            &schArticle.Blurb,
-		"Kicker":           &schArticle.Kicker,
-		"Body":             &schArticle.Body,
-		"LinkTitle":        &schArticle.LinkTitle,
-		"SuppressFeatured": &schArticle.SuppressFeatured,
-		"LastArcSync":      &schArticle.LastArcSync,
-		"LastSaved":        &schArticle.LastSaved,
-	}
-	if err := json.Unmarshal(dbArticle.SpotlightPAData, &spotlightPADataVal); err != nil {
-		return nil, err
-	}
-
-	if schArticle.LastArcSync.IsZero() {
-		if err := schArticle.ResetArcData(dbArticle); err != nil {
-			return nil, err
-		}
-	}
-	return &schArticle, nil
-}
-
 func nullString(s string) sql.NullString {
+	if s == "" {
+		return sql.NullString{}
+	}
 	return sql.NullString{String: s, Valid: true}
 }
 
@@ -211,38 +199,4 @@ func nullTime(t *time.Time) sql.NullTime {
 		return sql.NullTime{Time: *t, Valid: true}
 	}
 	return sql.NullTime{}
-}
-
-func ScheduledArticleToDB(schArticle *ScheduledArticle) (*db.Article, error) {
-	var dart db.Article
-	if schArticle.ArcID != "" {
-		dart.ArcID = nullString(schArticle.ArcID)
-	}
-	dart.ScheduleFor = nullTime(schArticle.ScheduleFor)
-	spotlightPADataVal := map[string]interface{}{
-		"ID":               &schArticle.ID,
-		"Budget":           &schArticle.Budget,
-		"ImageURL":         &schArticle.ImageURL,
-		"ImageCaption":     &schArticle.ImageCaption,
-		"ImageCredit":      &schArticle.ImageCredit,
-		"PubDate":          &schArticle.PubDate,
-		"Slug":             &schArticle.Slug,
-		"Authors":          &schArticle.Authors,
-		"Byline":           &schArticle.Byline,
-		"Hed":              &schArticle.Hed,
-		"Subhead":          &schArticle.Subhead,
-		"Summary":          &schArticle.Summary,
-		"Blurb":            &schArticle.Blurb,
-		"Kicker":           &schArticle.Kicker,
-		"Body":             &schArticle.Body,
-		"LinkTitle":        &schArticle.LinkTitle,
-		"SuppressFeatured": &schArticle.SuppressFeatured,
-		"LastArcSync":      &schArticle.LastArcSync,
-		"LastSaved":        &schArticle.LastSaved,
-	}
-	var err error
-	if dart.SpotlightPAData, err = json.Marshal(&spotlightPADataVal); err != nil {
-		return nil, err
-	}
-	return &dart, nil
 }
