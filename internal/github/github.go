54a7082efc1a1d1f0c2664d7702fb7a62c8df3d8
Carl Johnson <me@carlmjohnson.net>
2020-03-24 09:31:17 -0400

    Add Github updates

--- internal/github/github.go
+++ internal/github/github.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"flag"
 
-	"github.com/google/go-github/v29/github"
+	"github.com/google/go-github/v30/github"
 	"github.com/spotlightpa/almanack/pkg/almanack"
 	"golang.org/x/oauth2"
 )
@@ -23,18 +23,7 @@ func FlagVar(fl *flag.FlagSet) func(l almanack.Logger) (almanack.ContentStore, e
 		if *token == "" || *owner == "" || *repo == "" || *branch == "" {
 			return NewMockClient(l)
 		}
-		ctx := context.Background()
-		ts := oauth2.StaticTokenSource(
-			&oauth2.Token{AccessToken: *token},
-		)
-		tc := oauth2.NewClient(ctx, ts)
-
-		client := github.NewClient(tc)
-		cl := &Client{client, *owner, *repo, *branch, l}
-		if err := cl.Ping(ctx); err != nil {
-			return nil, err
-		}
-		return cl, nil
+		return NewClient(*token, *owner, *repo, *branch, l)
 	}
 }
 
@@ -42,6 +31,21 @@ type Client struct {
 	client              *github.Client
 	owner, repo, branch string
 	l                   almanack.Logger
+}
+
+func NewClient(token, owner, repo, branch string, l almanack.Logger) (*Client, error) {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+	cl := &Client{client, owner, repo, branch, l}
+	if err := cl.Ping(ctx); err != nil {
+		return nil, err
+	}
+	return cl, nil
 }
 
 func (cl *Client) printf(format string, v ...interface{}) {
@@ -66,6 +70,53 @@ func (cl *Client) CreateFile(ctx context.Context, msg, path string, content []by
 		},
 	}
 	_, _, err := cl.client.Repositories.CreateFile(ctx, cl.owner, cl.repo, path, opts)
+	return err
+}
+
+func (cl *Client) GetFile(ctx context.Context, path string) (contents string, err error) {
+	cl.printf("getting file %s from Github %s/%s@%s",
+		path, cl.owner, cl.repo, cl.branch)
+
+	fileInfo, _, _, err := cl.client.Repositories.GetContents(
+		ctx,
+		cl.owner,
+		cl.repo,
+		path,
+		&github.RepositoryContentGetOptions{Ref: cl.branch})
+	if err != nil {
+		return
+	}
+	contents, err = fileInfo.GetContent()
+	return
+}
+
+func (cl *Client) UpdateFile(ctx context.Context, msg, path string, content []byte) error {
+	cl.printf("updating file %s on Github %s/%s@%s",
+		path, cl.owner, cl.repo, cl.branch)
+
+	fileInfo, _, _, err := cl.client.Repositories.GetContents(
+		ctx,
+		cl.owner,
+		cl.repo,
+		path,
+		&github.RepositoryContentGetOptions{Ref: cl.branch})
+	if err != nil {
+		return err
+	}
+
+	opts := &github.RepositoryContentFileOptions{
+		Message: github.String(msg),
+		Content: content,
+		Branch:  github.String(cl.branch),
+		SHA:     fileInfo.SHA,
+		Author: &github.CommitAuthor{
+			Name:  github.String("Almanack"),
+			Email: github.String("webmaster@spotlightpa.org"),
+		},
+	}
+
+	_, _, err = cl.client.Repositories.UpdateFile(ctx, cl.owner, cl.repo, path, opts)
+
 	return err
 }
 
---
+++ internal/github/github_test.go
@@ -0,0 +1,59 @@
+package github_test
+
+import (
+	"bytes"
+	"context"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/spotlightpa/almanack/internal/github"
+)
+
+func check(t *testing.T, err error, msg string) {
+	t.Helper()
+	if err != nil {
+		t.Errorf("%s: %v", msg, err)
+	}
+}
+
+func eq(t *testing.T, want, have string) {
+	t.Helper()
+	if want != have {
+		t.Errorf("want %q; have %q", want, have)
+	}
+}
+
+func TestGithub(t *testing.T) {
+	token := os.Getenv("ALMANACK_GITHUB_TEST_TOKEN")
+	owner := os.Getenv("ALMANACK_GITHUB_TEST_OWNER")
+	repo := os.Getenv("ALMANACK_GITHUB_TEST_REPO")
+	branch := os.Getenv("ALMANACK_GITHUB_TEST_BRANCH")
+
+	if token == "" || owner == "" || repo == "" || branch == "" {
+		t.Skip("Missing Github ENV vars")
+	}
+	var buf bytes.Buffer
+	l := log.New(&buf, "", 0)
+	client, err := github.NewClient(token, owner, repo, branch, l)
+	check(t, err, "could not create client")
+	ctx := context.Background()
+	// create
+	testFileContents := time.Now().Format(time.Stamp)
+	fname := time.Now().Format("test-" + time.RFC3339 + ".txt")
+	err = client.CreateFile(ctx, "test create", fname, []byte(testFileContents))
+	check(t, err, "could not create file")
+	// get
+	returned, err := client.GetFile(ctx, fname)
+	check(t, err, "could not get file")
+	eq(t, testFileContents, string(returned))
+	// update
+	testFileContents = time.Now().Format(time.Stamp)
+	err = client.UpdateFile(ctx, "test update", fname, []byte(testFileContents))
+	check(t, err, "could not update file")
+	// get
+	returned, err = client.GetFile(ctx, fname)
+	check(t, err, "could not get file")
+	eq(t, testFileContents, string(returned))
+}
