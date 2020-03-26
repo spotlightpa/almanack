fc9c4c9cb07af34b38ae1f7a72a4fa499687b896
Carl Johnson <me@carlmjohnson.net>
2020-03-26 15:30:03 -0400

    PostCSS: Turn off purgecss
    Doesn't work with Buefy and Netlify widget?

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,4 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
-
 let plugins = [require("autoprefixer")];
-if (process.env.NODE_ENV === "production") {
-  plugins.push(
-    purgecss({
-      content: ["src/**/*.html", "src/**/*.vue"],
-    })
-  );
-}
 
 module.exports = {
   plugins,
