569a905c29125eae356542a7b13364ce1b20461b
Carl Johnson <me@carlmjohnson.net>
2020-04-01 15:31:50 -0400

    Image import script: Set uploaded true

--- sql/one-time/image-import.sql
+++ sql/one-time/image-import.sql
@@ -5,15 +5,21 @@ WITH input AS (
       $$ REPLACEME $$
       --
 ::jsonb) AS data)
-INSERT INTO image (path, credit, description, created_at)
+INSERT INTO image (path, credit, description, is_uploaded, created_at)
 SELECT
   data ->> 'image' AS path,
   min(data ->> 'image-credit'),
   coalesce(min(data ->> 'image-description'), ''),
+  TRUE,
   min(to_timestamp(data ->> 'created-at', 'YYYY-MM-DD"T"HH24:MI:SS"Z"'))
 FROM
   input
 GROUP BY
   path
 ON CONFLICT (path)
-  DO NOTHING;
+  DO UPDATE SET
+    description = coalesce(image.description, excluded.description),
+    created_at = least (image.created_at, excluded.created_at),
+    is_uploaded = TRUE
+  RETURNING
+    *;
