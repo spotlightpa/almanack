5b99b08dbf26be7722c208f1bff132bf015e990f
Carl Johnson <me@carlmjohnson.net>
2020-05-06 13:17:38 -0400

    Frontend: Add Sentry

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
+import * as Sentry from "@sentry/browser";
+
+let dsn =
+  process.env.NODE_ENV === "production"
+    ? "https://cf41d56053f841ae9625673c3ab8d53f@o361657.ingest.sentry.io/3944373"
+    : "";
+
+Sentry.init({ dsn });
+Sentry.captureException(new Error("Test error"));
+
 import Vue from "vue";
 import router from "./router.js";
 import "./filters/index.js";
