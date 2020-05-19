40cf6bd306e544acf9e53fa63c03ac17b1b33171
Carl Johnson <me@carlmjohnson.net>
2020-05-19 11:31:39 -0400

    Editors Picks refinements

--- src/components/ViewEditorsPicks.vue
+++ src/components/ViewEditorsPicks.vue
@@ -136,12 +136,6 @@ export default {
       Homepage Editor
     </h1>
 
-    <details class="content">
-      <summary>Help</summary>
-
-      <p>Lorem ipsum</p>
-    </details>
-
     <progress
       v-if="!didLoad && isLoading"
       class="progress is-large is-warning"
@@ -191,9 +185,42 @@ export default {
         Revert
       </button>
     </div>
-    <details>
-      <summary>debug</summary>
-      <code class="code">{{ edPicks }}</code>
+
+    <details class="content">
+      <summary>Help</summary>
+
+      <p>The Spotlight PA homepage is defined by three lists:</p>
+      <ul>
+        <li>
+          Homepage featured article: Defines what is in the first featured image
+          on the page.
+        </li>
+        <li>Subfeatures stories: Bulleted items under the featured story.</li>
+        <li>
+          Pinned stories: Other stories that will appear on the homepage,
+          regardless of date.
+        </li>
+      </ul>
+      <p>
+        Search for stories by slug, title, or author in the recent Spotlight PA
+        articles box. Stories must be saved in Almanack to be listed. Click a
+        story to add it to the featured list or drag it to a particular list.
+      </p>
+      <p>
+        All lists filter out <em>unpublished</em> stories automatically.
+        Duplicated items are removed automatically. For the featured slot and
+        restricted subfeatures, the last published items in the list win.
+      </p>
+      <p>
+        Example: Suppose SPL1, SPL2, SPL3, and SPL4 are all in the featured
+        article list and the subfeatures list, but SPL4 is not published yet.
+        Suppose also that the subfeatures list is limited to two items. In that
+        case, SPL3 will be in the top slot and SPL1 and SPL2 will be listed as
+        bulleted items. When SPL4 is published, it will take the featured slot,
+        and SPL2 and SPL3 will become bulleted items. In this way, you can
+        arrange the homepage so that it automatically changes as stories become
+        published.
+      </p>
     </details>
   </div>
 </template>
