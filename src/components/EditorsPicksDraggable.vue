4d899fed1301a7ab80b470a27b016ca6ef75ce5e
Carl Johnson <me@carlmjohnson.net>
2020-05-19 12:28:43 -0400

    Editors picks: Add user-select all

--- src/components/EditorsPicksDraggable.vue
+++ src/components/EditorsPicksDraggable.vue
@@ -34,7 +34,11 @@ export default {
     chosen-class="is-primary"
     :component-data="getComponentData()"
   >
-    <span v-for="(article, i) of value" :key="i" class="tag is-medium spacer">
+    <span
+      v-for="(article, i) of value"
+      :key="i"
+      class="tag is-medium spacer select-all"
+    >
       {{ article.internal_id }}
       <button class="delete" @click="remove(i)"></button>
     </span>
@@ -48,5 +52,8 @@ export default {
 .spacer {
   margin-right: 0.5rem;
   margin-bottom: 0.25rem;
+}
+.select-all {
+  user-select: all;
 }
 </style>
