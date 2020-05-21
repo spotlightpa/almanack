3890dfdb79abf72735a73754501846a55febc154
Carl Johnson <me@carlmjohnson.net>
2020-05-21 10:34:50 -0400

    Router: Send users without permissions to error page

---
+++ src/components/ViewUnauthorized.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="message is-warning">
+    <h1 class="message-header">Unauthorized User Account</h1>
+    <p class="message-body">
+      You have successfully registered for the Spotlight PA Almanack, but your
+      account has no access permissions associated with it. Please contact
+      <a href="mailto:cjohnson@spotlightpa.org">cjohnson@spotlightpa.org</a>
+      to request access.
+    </p>
+  </div>
+</template>
