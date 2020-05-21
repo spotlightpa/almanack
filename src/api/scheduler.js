c5aa3086d7d6cb9e55081a68966ad3bee1c77e84
Carl Johnson <me@carlmjohnson.net>
2020-05-21 10:58:33 -0400

    Scheduler: Tidy

--- src/api/scheduler.js
+++ src/api/scheduler.js
@@ -4,26 +4,21 @@ import { makeState } from "./service-util.js";
 import { useClient } from "./service.js";
 import ScheduledArticle from "./scheduled-article.js";
 
-function getScheduledArticle({ client, id }) {
+export function useScheduler(id) {
   const { apiState, exec } = makeState();
+  let client = useClient();
+
+  const load = () => exec(() => client.article(id));
+  load();
 
   return {
     ...toRefs(apiState),
+    load,
 
     article: computed(() =>
       apiState.rawData
         ? new ScheduledArticle({ client, id, data: apiState.rawData })
         : null
     ),
-
-    load() {
-      return exec(() => client.article(id));
-    },
   };
-}
-
-export function useScheduler(id) {
-  let obj = getScheduledArticle({ client: useClient(), id });
-  obj.load();
-  return obj;
 }
