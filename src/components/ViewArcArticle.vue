64845e9566263594374e060289e14a701eb896fb
Carl Johnson <me@carlmjohnson.net>
2020-06-11 16:38:40 -0400

    Rename ArcArticleAvailable

--- src/components/ViewArcArticle.vue
+++ src/components/ViewArcArticle.vue
@@ -1,13 +1,13 @@
 <script>
 import { useAvailableArc } from "@/api/hooks.js";
 import APILoader from "./APILoader.vue";
-import ArticleDetails from "./ArticleDetails.vue";
+import ArcArticleAvailable from "./ArcArticleAvailable.vue";
 
 export default {
   name: "ViewArcArticle",
   components: {
     APILoader,
-    ArticleDetails,
+    ArcArticleAvailable,
   },
   metaInfo() {
     return {
@@ -32,15 +32,11 @@ export default {
 
 <template>
   <APILoader :is-loading="isLoading" :reload="load" :error="error">
-    <div v-if="!article" class="message is-warning">
-      <p class="message-header">
-        Not found
-      </p>
-      <p class="message-body">
-        Article not found.
-        <router-link :to="{ name: 'home' }">Go home</router-link>?
-      </p>
-    </div>
-    <ArticleDetails v-else :article="article"></ArticleDetails>
+    <template v-if="article">
+      <ArcArticleAvailable
+        v-if="article.isAvailable"
+        :article="article"
+      ></ArcArticleAvailable>
+    </template>
   </APILoader>
 </template>
