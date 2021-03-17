c6a0047ed42ded50eb227962c5b49bad97ca7afa
Carl Johnson <me@carlmjohnson.net>
2021-03-17 19:31:51 -0400

    Build backend: Use embed rather than LDFLAGS

--- pkg/almanack/almanack.go
+++ pkg/almanack/almanack.go
@@ -1,6 +1,15 @@
 package almanack
 
+import (
+	_ "embed"
+	"strings"
+)
+
 var (
-	BuildVersion string = "Development"
-	DeployURL    string = "http://localhost"
+	//go:embed build-version.txt
+	buildVersion string
+	BuildVersion = strings.TrimSpace(buildVersion)
+	//go:embed deploy-url.txt
+	deployURL string
+	DeployURL = strings.TrimSpace(deployURL)
 )
