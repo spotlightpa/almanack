25b25d1b097504fd41a65663e024cf23c3691045
Carl Johnson <me@carlmjohnson.net>
2021-04-09 15:03:56 -0400

    Newsletters: Collect blurbs and descriptions

--- internal/db/models.go
+++ internal/db/models.go
@@ -79,6 +79,8 @@ type Newsletter struct {
 	Type        string    `json:"type"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
+	Description string    `json:"description"`
+	Blurb       string    `json:"blurb"`
 }
 
 type NewsletterType struct {
