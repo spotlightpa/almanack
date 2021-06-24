8329a0757a7ba0aa6af765ac65977c5ca6c91abd
Carl Johnson <me@carlmjohnson.net>
2021-06-23 21:48:05 -0400

    Models: Breakout page, add auto timestamp, add newsletter ID

---
+++ sql/schema/010_page.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "page" (
+  id bigserial PRIMARY KEY,
+  path text NOT NULL UNIQUE,
+  frontmatter jsonb NOT NULL DEFAULT '{}' ::jsonb,
+  body text NOT NULL DEFAULT '',
+  schedule_for timestamptz,
+  last_published timestamptz,
+  created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TRIGGER row_updated_at_on_page_trigger_
+  BEFORE UPDATE ON "page"
+  FOR EACH ROW
+  EXECUTE PROCEDURE update_row_updated_at_function_ ();
+
+---- create above / drop below ----
+DROP TABLE "page";
