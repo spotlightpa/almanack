21636a271e80bd3be44cbbbc75303603d98e48a5
Carl Johnson <me@carlmjohnson.net>
2021-08-19 15:37:08 -0400

    FE: move Page object to its own shared hook

--- src/components/ViewNewsletterPage.vue
+++ src/components/ViewNewsletterPage.vue
@@ -1,150 +1,21 @@
 <script>
-import Vue from "vue";
-import { computed, ref, toRefs, watch } from "@vue/composition-api";
+import { computed, toRefs } from "@vue/composition-api";
+import { usePage } from "../api/spotlightpa-page.js";
 
 import { formatDateTime } from "@/utils/time-format.js";
-
-import { makeState } from "@/api/service-util.js";
-import { useClient } from "@/api/client.js";
-import imgproxyURL from "@/api/imgproxy-url.js";
 
 import BulmaAutocompleteArray from "./BulmaAutocompleteArray.vue";
 import BulmaField from "./BulmaField.vue";
 import BulmaFieldInput from "./BulmaFieldInput.vue";
-
-class Page {
-  constructor(data) {
-    this.id = data["id"] ?? "";
-    this.body = data["body"] ?? "";
-    this.frontmatter = data["frontmatter"] ?? {};
-    this.filePath = data["file_path"] ?? "";
-    this.urlPath = data["url_path"]?.String ?? "";
-    this.createdAt = data["created_at"] ?? "";
-    this.publishedAt = Page.getDate(this.frontmatter, "published");
-    this.updatedAt = Page.getDate(data, "updated_at");
-    this.lastPublished = Page.getNullableDate(data, "last_published");
-    this.scheduleFor = Page.getNullableDate(data, "schedule_for");
-    this.kicker = this.frontmatter["kicker"] ?? "";
-    this.title = this.frontmatter["title"] ?? "";
-    this.internalID = this.frontmatter["internal-id"] ?? "";
-    this.linkTitle = this.frontmatter["linktitle"] ?? "";
-    this.titleTag = this.frontmatter["title-tag"] ?? "";
-    this.authors = this.frontmatter["authors"] ?? [];
-    this.byline = this.frontmatter["byline"] ?? "";
-    this.summary = this.frontmatter["description"] ?? "";
-    this.blurb = this.frontmatter["blurb"] ?? "";
-    this.topics = this.frontmatter["topics"] ?? [];
-    this.series = this.frontmatter["series"] ?? [];
-    this.image = this.frontmatter["image"] ?? "";
-    this.imageDescription = this.frontmatter["image-description"] ?? "";
-    this.imageCredit = this.frontmatter["image-credit"] ?? "";
-    this.imageSize = this.frontmatter["image-size"] ?? "";
-    this.languageCode = this.frontmatter["language-code"] ?? "";
-    this.slug = this.frontmatter["slug"] ?? "";
-    this.extendedKicker = this.frontmatter["extended-kicker"] ?? "";
-    this.modalExclude = this.frontmatter["modal-exclude"] ?? "";
-    this.noIndex = this.frontmatter["no-index"] ?? "";
-    this.overrideURL = this.frontmatter["url"] ?? "";
-    this.aliases = this.frontmatter["aliases"] ?? [];
-    this.layout = this.frontmatter["layout"] ?? "";
-
-    // not a getter so it won't react to changes
-    this.status = "pub";
-    if (!this.lastPublished) {
-      this.status = this.scheduleFor ? "sked" : "none";
-    }
-
-    Vue.observable(this);
-  }
-
-  static getDate(data, prop) {
-    let date = data[prop] ?? null;
-    return date && new Date(date);
-  }
-
-  static getNullableDate(data, prop) {
-    return data[prop]?.Valid ? new Date(data[prop].Time) : null;
-  }
-
-  get isPublished() {
-    return !!this.lastPublished;
-  }
-
-  get statusVerbose() {
-    return {
-      pub: "published",
-      sked: "scheduled to be published",
-      none: "unpublished",
-    }[this.status];
-  }
-
-  get link() {
-    if (!this.isPublished || !this.urlPath) {
-      return "";
-    }
-
-    return new URL(this.urlPath, "https://www.spotlightpa.org").href;
-  }
-
-  get imagePreviewURL() {
-    if (!this.image || this.image.match(/^http/)) {
-      return "";
-    }
-    return imgproxyURL(this.image);
-  }
-
-  toJSON() {
-    return {
-      file_path: this.filePath,
-      set_frontmatter: true,
-      frontmatter: {
-        // preserve unknown props
-        ...this.frontmatter,
-        // copy others
-        published: this.publishedAt,
-        kicker: this.kicker,
-        title: this.title,
-        "internal-id": this.internalID,
-        linktitle: this.linkTitle,
-        "title-tag": this.titleTag,
-        authors: this.authors,
-        byline: this.byline,
-        description: this.summary,
-        blurb: this.blurb,
-        topics: this.topics,
-        series: this.series,
-        image: this.image,
-        "image-description": this.imageDescription,
-        "image-credit": this.imageCredit,
-        "image-size": this.imageSize,
-        "language-code": this.languageCode,
-        slug: this.slug,
-        "extended-kicker": this.extendedKicker,
-        "modal-exclude": this.modalExclude,
-        "no-index": this.noIndex,
-        url: this.overrideURL,
-        aliases: this.aliases,
-        layout: this.layout,
-      },
-      set_body: false, // todo
-      body: "",
-      set_schedule_for: true,
-      schedule_for: {
-        Valid: !!this.scheduleFor,
-        Time: this.scheduleFor,
-      },
-      url_path: "", // leave blank to prevent changes
-      set_last_published: false,
-    };
-  }
-}
+import CopyWithButton from "./CopyWithButton.vue";
 
 export default {
-  name: "ViewNewsletterPage",
+  name: "ViewSpotlightPAPage",
   components: {
     BulmaAutocompleteArray,
     BulmaField,
     BulmaFieldInput,
+    CopyWithButton,
   },
   props: {
     id: String,
@@ -155,97 +26,18 @@ export default {
     };
   },
   setup(props) {
-    const showProgress = ref(false);
-    const toggleProgress = () => {
-      showProgress.value = true;
-      window.setTimeout(() => {
-        showProgress.value = false;
-      }, 1000);
-    };
-
-    const { getPage, postPage, listImages } = useClient();
-    const { apiState, exec } = makeState();
-
-    const fetch = (id) => {
-      toggleProgress();
-      return exec(() => getPage(id));
-    };
-    const post = (page) => {
-      toggleProgress();
-      return exec(() => postPage(page));
-    };
-    const page = computed(() =>
-      apiState.rawData ? new Page(apiState.rawData) : null
-    );
-
-    watch(() => props.id, fetch, {
-      immediate: true,
-    });
-
-    const { apiState: imageState, exec: execImage } = makeState();
-    execImage(() => listImages());
+    const { id } = toRefs(props);
+    const pageData = usePage(id);
 
     return {
-      showProgress,
-      showScheduler: ref(false),
-
-      ...toRefs(apiState),
+      ...pageData,
       formatDateTime,
-
-      fetch,
-      post,
-      page,
-
       title: computed(() => {
-        if (!page.value) {
-          return `Newsletter Page ${props.id}`;
+        if (!pageData.page.value) {
+          return `Newsletter Page ${id.value}`;
         }
-        return "Page " + (page.value.internalID || "Untitled");
+        return "Page " + (pageData.page.value.internalID || "Untitled");
       }),
-      deriveSlug() {
-        page.value.slug = page.value.title
-          .toLowerCase()
-          .replace(/\b(the|an?)\b/g, " ")
-          .replace(/\bpa\b/g, "pennsylvania")
-          .replace(/’/g, "'")
-          .replace(/.?'s/g, "s")
-          .replace(/\W+/g, " ")
-          .trim()
-          .replace(/ /g, "-");
-      },
-      discardChanges() {
-        if (window.confirm("Do you really want to discard all changes?")) {
-          fetch(props.id);
-        }
-      },
-      publishNow() {
-        page.value.scheduleFor = new Date();
-        return post(page.value);
-      },
-      updateSchedule() {
-        const msg =
-          "Scheduled publication date is in the past. Do you want to publish now?";
-        let isPostDated = page.value.scheduleFor - new Date() > 0;
-        if (!isPostDated && !window.confirm(msg)) {
-          return;
-        }
-        return post(page.value);
-      },
-      updateOnly() {
-        page.value.scheduleFor = null;
-        return post(page.value);
-      },
-
-      imageState,
-      images: computed(() =>
-        !imageState.rawData ? [] : imageState.rawData.images
-      ),
-      imgproxyURL,
-      setImageProps(image) {
-        page.value.image = image.path;
-        page.value.imageDescription = image.description;
-        page.value.imageCredit = image.credit;
-      },
     };
   },
 };
@@ -494,7 +286,9 @@ export default {
         Derive slug from title
       </button>
 
-      <div v-if="page.link" class="buttons">
+      <CopyWithButton v-if="page.link" :value="page.link" label="Page URL" />
+
+      <div v-if="page.isPublished && page.link" class="buttons">
         <a
           :href="page.link"
           class="button is-success has-text-weight-semibold"
