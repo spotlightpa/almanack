6079530fed069feb6aee0e1b6f54dbefc987ff7a
Carl Johnson <me@carlmjohnson.net>
2021-09-09 10:26:05 -0400

    PGX: Fix ErrNoRows

--- internal/db/errs.go
+++ internal/db/errs.go
@@ -1,16 +1,16 @@
 package db
 
 import (
-	"database/sql"
 	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/carlmjohnson/resperr"
+	"github.com/jackc/pgx/v4"
 )
 
 func IsNotFound(err error) bool {
-	return errors.Is(err, sql.ErrNoRows)
+	return errors.Is(err, pgx.ErrNoRows)
 }
 
 func NoRowsAs404(err error, format string, a ...interface{}) error {
