3eed4a438d3edbd3d1fa3eea6da5765c033fc1ab
Carl Johnson <me@carlmjohnson.net>
2022-06-06 13:41:07 -0400

    Add LinkButton(s); group menu items

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  props: {
+    to: String,
+    label: String,
+    icon: Array,
+    size: {
+      type: String,
+      default: "is-small",
+    },
+    color: {
+      type: String,
+      default: "is-light",
+    },
+  },
+};
+</script>
+
+<template>
+  <router-link
+    :to="{ name: to }"
+    class="button has-text-weight-semibold"
+    :class="[size, color]"
+  >
+    <span v-if="icon" class="icon">
+      <font-awesome-icon :icon="icon" />
+    </span>
+    <span v-text="label" />
+  </router-link>
+</template>
