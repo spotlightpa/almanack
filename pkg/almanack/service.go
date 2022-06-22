6b9ea684288f32ec1337b115c2682d74856343c7
Carl Johnson <me@carlmjohnson.net>
2022-06-21 21:34:33 -0400

    Common: Move ContentStore out

--- pkg/almanack/service.go
+++ pkg/almanack/service.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/spotlightpa/almanack/internal/aws"
 	"github.com/spotlightpa/almanack/internal/db"
+	"github.com/spotlightpa/almanack/internal/github"
 	"github.com/spotlightpa/almanack/internal/google"
 	"github.com/spotlightpa/almanack/internal/index"
 	"github.com/spotlightpa/almanack/internal/mailchimp"
@@ -16,7 +17,7 @@ type Service struct {
 	common.Logger
 	Client  *http.Client
 	Queries *db.Queries
-	common.ContentStore
+	github.ContentStore
 	ImageStore       aws.BlobStore
 	FileStore        aws.BlobStore
 	SlackClient      slack.Client
