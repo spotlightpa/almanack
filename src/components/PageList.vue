be3256447a983ced999cdce146ce147f18f5d70f
Carl Johnson <me@carlmjohnson.net>
2022-06-27 12:45:04 -0400

    Extract PageList.vue

---
+++ src/components/PageList.vue
@@ -0,0 +1,74 @@
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    page: {},
+    nextPage: {
+      type: Object,
+    },
+    apiState: {
+      type: Object,
+      required: true,
+    },
+    reload: {
+      type: Function,
+      required: true,
+    },
+    pages: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup(props) {
+    return {
+      ...props.apiState,
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <BulmaBreadcrumbs
+      :links="[
+        { name: 'Admin', to: { name: 'admin' } },
+        { name: title, to: {} },
+      ]"
+    />
+    <h1 class="title">
+      {{ title }}
+      <template v-if="page">(overflow page {{ page }})</template>
+    </h1>
+    <APILoader :is-loading="isLoading" :reload="reload" :error="error">
+      <table class="table is-striped is-narrow is-fullwidth">
+        <tbody>
+          <PageListRow
+            v-for="page of pages"
+            :key="page.id"
+            :link="page.link"
+            :status="page.status"
+            :label="page.internalID"
+            :date="page.publishedAt"
+            :hed="page.title"
+            :dek="page.blurb"
+            :image="page.image"
+            :image-alt="page.image"
+          />
+        </tbody>
+      </table>
+
+      <div class="buttons mt-5">
+        <router-link
+          v-if="nextPage"
+          :to="nextPage"
+          class="button is-primary has-text-weight-semibold"
+        >
+          Show Older Stories…
+        </router-link>
+      </div>
+    </APILoader>
+  </div>
+</template>
