dbd34648df283844111eb4460caab588ad7a6334
Carl Johnson <me@carlmjohnson.net>
2022-06-28 21:10:08 -0400

    BE: Colorize logger

--- pkg/common/httplogger.go
+++ pkg/common/httplogger.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/carlmjohnson/requests"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func init() {
@@ -12,8 +13,22 @@ func init() {
 	http.DefaultTransport = requests.RoundTripFunc(func(req *http.Request) (res *http.Response, err error) {
 		start := time.Now()
 		res, err = oldTransport.RoundTrip(req)
-		Logger.Printf("request to %s %q took %v",
-			req.Method, req.Host, time.Since(start))
+		Logger.Printf("request to %s%s%s %s%q%s took %s%v%s",
+			purple, req.Method, reset, cyan, req.Host, reset, yellow, time.Since(start), reset)
 		return
 	})
+}
+
+var (
+	yellow = maybe("\033[33m")
+	purple = maybe("\033[35;1m")
+	cyan   = maybe("\033[36m")
+	reset  = maybe("\033[0m")
+)
+
+func maybe(s string) string {
+	if !middleware.IsTTY {
+		return ""
+	}
+	return s
 }
