bfb58c5caa9f26cde6355cfb76a58097e978fad7
Carl Johnson <me@carlmjohnson.net>
2022-06-28 21:10:08 -0400

    BE: Colorize logger

--- internal/db/logger.go
+++ internal/db/logger.go
@@ -7,6 +7,7 @@ import (
 	"runtime"
 	"time"
 
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4"
 	"github.com/spotlightpa/almanack/internal/stringx"
@@ -38,6 +39,20 @@ func (l logger) QueryRow(ctx context.Context, query string, args ...any) pgx.Row
 	return row
 }
 
+var (
+	yellow = maybe("\033[33m")
+	purple = maybe("\033[35;1m")
+	cyan   = maybe("\033[36m")
+	reset  = maybe("\033[0m")
+)
+
+func maybe(s string) string {
+	if !middleware.IsTTY {
+		return ""
+	}
+	return s
+}
+
 func (l logger) log(kind string, d time.Duration) {
 	pc, file, line, ok := runtime.Caller(2)
 	prefix := "unknown function"
@@ -47,5 +62,7 @@ func (l logger) log(kind string, d time.Duration) {
 		_, name, _ := stringx.LastCut(f.Name(), ".")
 		prefix = fmt.Sprintf("%s(%s:%d)", name, file, line)
 	}
-	common.Logger.Printf("%s %s in %v", kind, prefix, d)
+	common.Logger.Printf("%s%s%s %s%s%s in %s%v%s",
+		purple, kind, reset, cyan, prefix, reset, yellow, d, reset,
+	)
 }
