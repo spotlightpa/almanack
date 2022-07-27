b43afad38cfa29fa1adfdcd68bb1fcdb138394bc
Carl Johnson <me@carlmjohnson.net>
2022-07-27 13:11:58 -0400

    Vite: Use import.meta.env instead of process.env

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/browser";
 
 let dsn =
-  process.env.NODE_ENV === "production"
+  import.meta.env.NODE_ENV === "production"
     ? "https://cf41d56053f841ae9625673c3ab8d53f@o361657.ingest.sentry.io/3944373"
     : "";
 
