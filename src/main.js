8127def37240e5323d3b790e2fee372c34c5c9d9
Carl Johnson <me@carlmjohnson.net>
2022-07-28 20:38:31 -0400

    Vue 3: Fix Sentry

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/browser";
 
 let dsn =
-  import.meta.env.NODE_ENV === "production"
+  import.meta.env.MODE === "production"
     ? "https://cf41d56053f841ae9625673c3ab8d53f@o361657.ingest.sentry.io/3944373"
     : "";
 
