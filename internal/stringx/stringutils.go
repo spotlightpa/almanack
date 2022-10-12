a4111fedfe0961e0015d75065fe32434c840a78c
Carl Johnson <me@carlmjohnson.net>
2022-10-11 21:04:40 -0400

    stringx: Docs, laziness

--- internal/stringx/stringutils.go
+++ internal/stringx/stringutils.go
@@ -1,11 +1,14 @@
+// Package stringx has string utilities
 package stringx
 
 import (
 	"regexp"
 	"strings"
+
+	"github.com/spotlightpa/almanack/internal/syncx"
 )
 
-// Return first non-blank string
+// First non-blank string
 func First(ss ...string) string {
 	for _, s := range ss {
 		if s != "" {
@@ -15,21 +18,28 @@ func First(ss ...string) string {
 	return ""
 }
 
+func mustBeLazy(s string) func() *regexp.Regexp {
+	return syncx.Once(
+		func() *regexp.Regexp {
+			return regexp.MustCompile(s)
+		})
+}
+
 var (
-	articleRe   = regexp.MustCompile(`\b(the|an?)\b`)
-	pennRe      = regexp.MustCompile(`\bpa\b`)
-	possesiveRe = regexp.MustCompile(`\.?[’']s`)
-	nonasciiRe  = regexp.MustCompile(`\W+`)
+	articleRe   = mustBeLazy(`\b(the|an?)\b`)
+	pennRe      = mustBeLazy(`\bpa\b`)
+	possesiveRe = mustBeLazy(`\.?[’']s`)
+	nonasciiRe  = mustBeLazy(`\W+`)
 )
 
 func Slugify(s string) string {
 	s = strings.ToLower(s)
-	s = articleRe.ReplaceAllString(s, " ")
-	s = pennRe.ReplaceAllString(s, "pennsylvania")
-	s = possesiveRe.ReplaceAllString(s, "s")
-	s = nonasciiRe.ReplaceAllString(s, " ")
+	s = articleRe().ReplaceAllString(s, " ")
+	s = pennRe().ReplaceAllString(s, "pennsylvania")
+	s = possesiveRe().ReplaceAllString(s, "s")
+	s = nonasciiRe().ReplaceAllString(s, " ")
 	s = strings.TrimSpace(s)
-	s = nonasciiRe.ReplaceAllString(s, "-")
+	s = nonasciiRe().ReplaceAllString(s, "-")
 	return s
 }
 
