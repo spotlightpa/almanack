78b4cbf6fb473596dccc4de39659f2c1d022d1d0
Carl Johnson <me@carlmjohnson.net>
2023-01-23 16:24:51 -0500

    Add SiteParamsFeatured

---
+++ src/components/SiteParamsFeatured.vue
@@ -0,0 +1,75 @@
+<script setup>
+import useData from "@/utils/use-data.js";
+import { toRel, toAbs } from "@/utils/link.js";
+
+const props = defineProps({
+  params: Object,
+  fileProps: Object,
+});
+
+const data = useData(() => props.params.data, {
+  featuredPromoLink: ["featured-promo-link", toAbs, toRel],
+  featuredPromoImageDescription: ["featured-promo-image-description"],
+  featuredPromoBgColor: ["featured-promo-bg-color"],
+  featuredPromoWidth: ["featured-promo-width"],
+  featuredPromoHeight: ["featured-promo-height"],
+  featuredPromoImages: ["featured-promo-images"],
+});
+</script>
+
+<template>
+  <details class="mt-4">
+    <summary class="title is-4">Featured layout promo</summary>
+    <p class="mb-4 content">
+      Featured layout promo is a large house ad in articles using the featured
+      layout.
+    </p>
+    <BulmaFieldInput
+      v-model="data.featuredPromoLink.value"
+      label="Featured promo link"
+      type="url"
+      help="Defaults to https://www.spotlightpa.org/donate/"
+    />
+
+    <BulmaTextarea
+      v-model="data.featuredPromoImageDescription.value"
+      label="Featured layout promo image description"
+      help="For blind readers and search engines"
+    />
+
+    <BulmaFieldColor
+      v-model="data.featuredPromoBgColor.value"
+      label="Featured layout promo background color"
+    />
+    <div class="is-flex mb-2">
+      <BulmaField v-slot="{ idForLabel }" label="Image width">
+        <input
+          :id="idForLabel"
+          v-model.number="data.featuredPromoWidth.value"
+          class="input"
+          inputmode="numeric"
+        />
+      </BulmaField>
+      <BulmaField v-slot="{ idForLabel }" class="ml-2" label="Image height">
+        <input
+          :id="idForLabel"
+          v-model.number="data.featuredPromoHeight.value"
+          class="input"
+          inputmode="numeric"
+        />
+      </BulmaField>
+    </div>
+
+    <BulmaField
+      label="Images"
+      help="If multiple images are provided, each page load will select one randomly"
+    >
+      <SiteParamsFiles
+        :files="data.featuredPromoImages.value"
+        :file-props="fileProps"
+        @add="data.featuredPromoImages.value.push($event)"
+        @remove="data.featuredPromoImages.value.splice($event, 1)"
+      />
+    </BulmaField>
+  </details>
+</template>
