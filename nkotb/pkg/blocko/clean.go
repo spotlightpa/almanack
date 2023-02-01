c3059250f9f5fdfd6dfeb8a23f28631cea3c99d1
Carl Johnson <me@carlmjohnson.net>
2023-02-01 10:17:52 -0500

    Blocko: Do cleaning before Markdownification

---
+++ nkotb/pkg/blocko/clean.go
@@ -0,0 +1,50 @@
+package blocko
+
+import (
+	"github.com/spotlightpa/nkotb/pkg/xhtml"
+	"golang.org/x/exp/slices"
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+)
+
+func Clean(root *html.Node) {
+	mergeSiblings(root)
+	removeEmptyP(root)
+}
+
+func mergeSiblings(root *html.Node) {
+	// find all matches first
+	var inlineSiblings []*html.Node
+	xhtml.VisitAll(root, func(n *html.Node) {
+		brother := n.NextSibling
+		if brother == nil ||
+			!xhtml.InlineElements[n.DataAtom] ||
+			n.DataAtom != brother.DataAtom ||
+			!slices.Equal(n.Attr, brother.Attr) {
+			return
+		}
+		inlineSiblings = append(inlineSiblings, n)
+	})
+	// then do mutation.
+	// no mutating while iterating!
+	// go in reverse order
+	// in case there are several siblings to merge
+	for i := len(inlineSiblings) - 1; i >= 0; i-- {
+		n := inlineSiblings[i]
+		xhtml.AdoptChildren(n, n.NextSibling)
+		n.Parent.RemoveChild(n.NextSibling)
+	}
+}
+
+func removeEmptyP(root *html.Node) {
+	var emptyP []*html.Node
+	xhtml.VisitAll(root, func(n *html.Node) {
+		if n.DataAtom == atom.P && xhtml.IsEmpty(n) {
+			emptyP = append(emptyP, n)
+			return
+		}
+	})
+	for _, n := range emptyP {
+		n.Parent.RemoveChild(n)
+	}
+}
