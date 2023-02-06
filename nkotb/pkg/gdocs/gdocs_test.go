b9d3204a064e2f1ad3da3db5d7265a85d93b6e6c
Carl Johnson <me@carlmjohnson.net>
2023-02-06 16:43:59 -0500

    GDocs: Test full conversion

--- nkotb/pkg/gdocs/gdocs_test.go
+++ nkotb/pkg/gdocs/gdocs_test.go
@@ -6,17 +6,18 @@ import (
 	"testing"
 
 	"github.com/carlmjohnson/be"
+	"github.com/spotlightpa/nkotb/pkg/blocko"
 	"github.com/spotlightpa/nkotb/pkg/testfile"
 	"github.com/spotlightpa/nkotb/pkg/xhtml"
 	"google.golang.org/api/docs/v1"
 )
 
 func TestConvert(t *testing.T) {
-	testfile.GlobRun(t, "testdata/*.html", func(path string, t *testing.T) {
-		bareName := strings.TrimSuffix(path, ".html")
-		want := testfile.Read(t, path)
+	testfile.GlobRun(t, "testdata/*.json", func(path string, t *testing.T) {
+		bareName := strings.TrimSuffix(path, ".json")
+		want := testfile.Read(t, bareName+".html")
 
-		s := testfile.Read(t, bareName+".json")
+		s := testfile.Read(t, path)
 
 		var doc docs.Document
 		be.NilErr(t, json.Unmarshal([]byte(s), &doc))
@@ -25,6 +26,27 @@ func TestConvert(t *testing.T) {
 
 		be.Debug(t, func() {
 			badname := bareName + "-bad.html"
+			testfile.Write(t, badname, got)
+		})
+		be.Equal(t, string(want), got)
+	})
+}
+
+func TestFullConvert(t *testing.T) {
+	testfile.GlobRun(t, "testdata/*.json", func(path string, t *testing.T) {
+		bareName := strings.TrimSuffix(path, ".json")
+		want := testfile.Read(be.Relaxed(t), bareName+".md")
+
+		s := testfile.Read(t, path)
+
+		var doc docs.Document
+		be.NilErr(t, json.Unmarshal([]byte(s), &doc))
+		n := Convert(&doc)
+		got, err := blocko.HTMLToMarkdown(xhtml.ToString(n))
+		be.NilErr(t, err)
+
+		be.Debug(t, func() {
+			badname := bareName + "-bad.md"
 			testfile.Write(t, badname, got)
 		})
 		be.Equal(t, string(want), got)
