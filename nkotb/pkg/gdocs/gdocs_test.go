e3472600693554460c6672a2a0c06d2c210f7633
Carl Johnson <me@carlmjohnson.net>
2023-02-06 13:36:21 -0500

    gdocs: Use be for tests

--- nkotb/pkg/gdocs/gdocs_test.go
+++ nkotb/pkg/gdocs/gdocs_test.go
@@ -1,43 +1,36 @@
 package gdocs
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"os"
 	"testing"
 
-	"golang.org/x/net/html"
+	"github.com/carlmjohnson/be"
+	"github.com/spotlightpa/nkotb/pkg/xhtml"
 	"google.golang.org/api/docs/v1"
 )
-
-func nilErr(t *testing.T, err error) {
-	if err != nil {
-		t.Fatalf("want nil; got %v", err)
-	}
-}
 
 func TestConvert(t *testing.T) {
 	for _, tc := range []string{
 		"example",
 	} {
+		want, err := os.ReadFile(fmt.Sprintf("testdata/%s.html", tc))
+		be.NilErr(t, err)
+
 		b, err := os.ReadFile(fmt.Sprintf("testdata/%s.json", tc))
-		nilErr(t, err)
+		be.NilErr(t, err)
 
 		var doc docs.Document
-		nilErr(t, json.Unmarshal(b, &doc))
+		be.NilErr(t, json.Unmarshal(b, &doc))
 		n := Convert(&doc)
-		var buf bytes.Buffer
-		nilErr(t, html.Render(&buf, n))
+		got := xhtml.ToString(n)
 
-		want, err := os.ReadFile(fmt.Sprintf("testdata/%s.html", tc))
-		nilErr(t, err)
-
-		if buf.String() != string(want) {
+		be.Debug(t, func() {
 			badname := fmt.Sprintf("testdata/%s-bad.html", tc)
-			os.WriteFile(badname, buf.Bytes(), 0644)
-			t.Fatalf("unexpected output see %s", badname)
-		}
+			os.WriteFile(badname, []byte(got), 0644)
+		})
+		be.Equal(t, string(want), got)
 	}
 
 }
