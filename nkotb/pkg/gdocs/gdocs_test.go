3821026ab104c43c9759005e0e2a2adf31862847
Carl Johnson <me@carlmjohnson.net>
2023-02-08 14:18:54 -0500

    Add benchmarks; use a bytes.Buffer pool

--- nkotb/pkg/gdocs/gdocs_test.go
+++ nkotb/pkg/gdocs/gdocs_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/spotlightpa/nkotb/pkg/blocko"
 	"github.com/spotlightpa/nkotb/pkg/testfile"
 	"github.com/spotlightpa/nkotb/pkg/xhtml"
+	"golang.org/x/net/html"
 	"google.golang.org/api/docs/v1"
 )
 
@@ -28,11 +29,12 @@ func TestConvert(t *testing.T) {
 			badname := bareName + "-bad.html"
 			testfile.Write(t, badname, got)
 		})
-		be.Equal(t, string(want), got)
+		be.Equal(t, want, got)
 	})
 }
 
 func TestFullConvert(t *testing.T) {
+	t.Parallel()
 	testfile.GlobRun(t, "testdata/*.json", func(path string, t *testing.T) {
 		bareName := strings.TrimSuffix(path, ".json")
 		want := testfile.Read(be.Relaxed(t), bareName+".md")
@@ -49,6 +51,36 @@ func TestFullConvert(t *testing.T) {
 			badname := bareName + "-bad.md"
 			testfile.Write(t, badname, got)
 		})
-		be.Equal(t, string(want), got)
+		be.Equal(t, want, got)
 	})
+}
+
+func BenchmarkConvert(b *testing.B) {
+	want := testfile.Read(be.Relaxed(b), "testdata/privacy.html")
+	s := testfile.Read(b, "testdata/privacy.json")
+	var got *html.Node
+	var doc docs.Document
+	be.NilErr(b, json.Unmarshal([]byte(s), &doc))
+
+	for i := 0; i < b.N; i++ {
+		got = Convert(&doc)
+	}
+	be.Equal(b, want, xhtml.ToString(got))
+}
+
+func BenchmarkFullConvert(b *testing.B) {
+	want := testfile.Read(be.Relaxed(b), "testdata/privacy.md")
+	s := testfile.Read(b, "testdata/privacy.json")
+	var got string
+
+	var doc docs.Document
+	be.NilErr(b, json.Unmarshal([]byte(s), &doc))
+
+	for i := 0; i < b.N; i++ {
+		n := Convert(&doc)
+		var err error
+		got, err = blocko.HTMLToMarkdown(xhtml.ToString(n))
+		be.NilErr(b, err)
+	}
+	be.Equal(b, want, got)
 }
