06bf675422c461db093609dbc33397879c05bb31
Carl Johnson <me@carlmjohnson.net>
2023-02-22 14:18:31 -0500

    main.js: Don't use Sentry in dev
    It just breaks the console

--- src/main.js
+++ src/main.js
@@ -4,12 +4,11 @@ const app = createApp(TheApp);
 
 import * as Sentry from "@sentry/vue";
 
-let dsn =
-  import.meta.env.MODE === "production"
-    ? "https://cf41d56053f841ae9625673c3ab8d53f@o361657.ingest.sentry.io/3944373"
-    : "";
-
-Sentry.init({ app, dsn });
+if (import.meta.env.MODE === "production") {
+  let dsn =
+    "https://cf41d56053f841ae9625673c3ab8d53f@o361657.ingest.sentry.io/3944373";
+  Sentry.init({ app, dsn });
+}
 
 import { Head, createHead } from "@vueuse/head";
 const head = createHead();
