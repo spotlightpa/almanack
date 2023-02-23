f74fd23b4c5c1a954f6934f832428ead7e7ff103
Carl Johnson <me@carlmjohnson.net>
2023-02-23 14:51:01 -0500

    Upgrade to sqlc 1.17.2

--- internal/db/null.go
+++ internal/db/null.go
@@ -8,13 +8,3 @@ var (
 	NullTime = pgtype.Timestamptz{}
 	NullText = pgtype.Text{}
 )
-
-func Array[T any](elems ...T) pgtype.Array[T] {
-	return pgtype.Array[T]{
-		Elements: elems,
-		Dims: []pgtype.ArrayDimension{
-			{Length: int32(len(elems)), LowerBound: 1},
-		},
-		Valid: true,
-	}
-}
