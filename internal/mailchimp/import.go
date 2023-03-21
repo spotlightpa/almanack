8d8183705d6f7671d39902521f65c6e6ac0c266f
Carl Johnson <me@carlmjohnson.net>
2023-03-21 13:31:53 -0400

    mailchimp: Use xhtml helpers

--- internal/mailchimp/import.go
+++ internal/mailchimp/import.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/carlmjohnson/errorx"
 	"github.com/carlmjohnson/requests"
+	"github.com/spotlightpa/almanack/internal/xhtml"
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
 )
@@ -29,70 +29,24 @@ func ImportPage(ctx context.Context, cl *http.Client, page string) (body string,
 }
 
 func PageContent(doc *html.Node) (body string, err error) {
-	var bNode *html.Node
-	if !findNode(doc, func(n *html.Node) bool {
-		if n.DataAtom != atom.Body {
-			return false
-		}
-		bNode = n
-		return true
-	}) {
+	bNode := xhtml.Find(doc, xhtml.WithBody)
+	if bNode == nil {
 		err = fmt.Errorf("could not find body element")
 		return
 	}
-	var remove [][2]*html.Node
-	visitAll(bNode, func(n *html.Node) {
-		if n.Type != html.CommentNode &&
-			n.DataAtom != atom.Style &&
-			n.DataAtom != atom.Script &&
-			!idEq(n, "awesomewrap") {
-			return
-		}
-		if p := n.Parent; p != nil {
-			remove = append(remove, [2]*html.Node{p, n})
-		}
+
+	remove := xhtml.FindAll(bNode, func(n *html.Node) bool {
+		return n.Type == html.CommentNode ||
+			n.DataAtom == atom.Style ||
+			n.DataAtom == atom.Script ||
+			xhtml.Attr(n, "id") == "awesomewrap"
 	})
-	for i := range remove {
-		p, c := remove[i][0], remove[i][1]
+
+	for _, c := range remove {
+		p := c.Parent
 		p.RemoveChild(c)
 	}
-	var buf strings.Builder
-	for n := bNode.FirstChild; n != nil; n = n.NextSibling {
-		if err = html.Render(&buf, n); err != nil {
-			return
-		}
-	}
-	body = buf.String()
+
+	body = xhtml.ContentsToString(bNode)
 	return
-}
-
-func idEq(n *html.Node, id string) bool {
-	if n.Type != html.ElementNode {
-		return false
-	}
-	for _, attr := range n.Attr {
-		if attr.Key == "id" && attr.Val == id {
-			return true
-		}
-	}
-	return false
-}
-
-func findNode(n *html.Node, callback func(*html.Node) bool) bool {
-	if callback(n) {
-		return true
-	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		if findNode(c, callback) {
-			return true
-		}
-	}
-	return false
-}
-
-func visitAll(n *html.Node, callback func(*html.Node)) {
-	findNode(n, func(n *html.Node) bool {
-		callback(n)
-		return false
-	})
 }
