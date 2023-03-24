88d81bf4af92796acfa5fe1b3f9d1450dc9618af
Carl Johnson <me@carlmjohnson.net>
2023-03-23 21:44:40 -0400

    GdocsClient: Add scope for downloading images

--- internal/google/docs.go
+++ internal/google/docs.go
@@ -13,11 +13,16 @@ func (gsvc *Service) GdocsClient(ctx context.Context) (cl *http.Client, err erro
 	if len(gsvc.cert) == 0 {
 		l := almlog.FromContext(ctx)
 		l.Warn("using default Google credentials")
-		cl, err = google.DefaultClient(ctx, "https://www.googleapis.com/auth/documents.readonly")
+		cl, err = google.DefaultClient(ctx,
+			"https://www.googleapis.com/auth/documents.readonly",
+			"https://www.googleapis.com/auth/drive.readonly",
+		)
 		return
 	}
 	creds, err := google.CredentialsFromJSON(
-		ctx, gsvc.cert, "https://www.googleapis.com/auth/documents.readonly",
+		ctx, gsvc.cert,
+		"https://www.googleapis.com/auth/documents.readonly",
+		"https://www.googleapis.com/auth/drive.readonly",
 	)
 	if err != nil {
 		return
