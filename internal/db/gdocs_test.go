ffcafee682cf7b6a06e3a2a7acfd26e96dc0d3d2
Carl Johnson <me@carlmjohnson.net>
2023-03-29 14:12:17 -0400

    TestProcessGDocsDoc: Use glob subtests

--- internal/db/gdocs_test.go
+++ internal/db/gdocs_test.go
@@ -20,46 +20,48 @@ func TestProcessGDocsDoc(t *testing.T) {
 	p := createTestDB(t)
 	q := db.New(p)
 	svc := almanack.Services{
-		Client: &http.Client{
-			Transport: requests.Replay("testdata"),
-		},
 		Queries:    q,
 		Tx:         db.NewTxable(p),
 		ImageStore: aws.NewBlobStore("mem://"),
 		FileStore:  aws.NewBlobStore("mem://"),
 	}
 	ctx := context.Background()
-	var doc docs.Document
-	testfile.Unmarshal(t, "testdata/example-doc.json", &doc)
-	dbDoc, err := svc.Queries.CreateGDocsDoc(ctx, db.CreateGDocsDocParams{
-		GDocsID:  "abc123",
-		Document: doc,
-	})
-	be.NilErr(t, err)
-	err = svc.ProcessGDocsDoc(ctx, dbDoc)
-	be.NilErr(t, err)
-	dbDoc, err = svc.Queries.GetGDocsByID(ctx, dbDoc.ID)
-	be.NilErr(t, err)
-
-	rawHTML := testfile.Read(be.Relaxed(t), "testdata/raw.html")
-	be.Debug(t, func() {
-		if rawHTML != dbDoc.RawHtml {
-			testfile.Write(t, "testdata/raw-bad.html", dbDoc.RawHtml)
+	testfile.GlobRun(t, "testdata/gdoc*", func(path string, t *testing.T) {
+		svc.Client = &http.Client{
+			Transport: requests.Replay(path),
 		}
-	})
-	be.Equal(t, rawHTML, dbDoc.RawHtml)
+		var doc docs.Document
+		testfile.Unmarshal(t, path+"/doc.json", &doc)
+		dbDoc, err := svc.Queries.CreateGDocsDoc(ctx, db.CreateGDocsDocParams{
+			GDocsID:  "abc123",
+			Document: doc,
+		})
+		be.NilErr(t, err)
+		err = svc.ProcessGDocsDoc(ctx, dbDoc)
+		be.NilErr(t, err)
+		dbDoc, err = svc.Queries.GetGDocsByID(ctx, dbDoc.ID)
+		be.NilErr(t, err)
 
-	richText := testfile.Read(be.Relaxed(t), "testdata/rich.html")
-	be.Debug(t, func() {
-		if richText != dbDoc.RichText {
-			testfile.Write(t, "testdata/rich-bad.html", dbDoc.RichText)
-		}
-	})
-	be.Equal(t, richText, dbDoc.RichText)
+		rawHTML := testfile.Read(be.Relaxed(t), path+"/raw.html")
+		be.Debug(t, func() {
+			if rawHTML != dbDoc.RawHtml {
+				testfile.Write(t, path+"/raw-bad.html", dbDoc.RawHtml)
+			}
+		})
+		be.Equal(t, rawHTML, dbDoc.RawHtml)
 
-	md := testfile.Read(be.Relaxed(t), "testdata/text.md")
-	be.Debug(t, func() {
-		testfile.Write(t, "testdata/text-bad.md", dbDoc.ArticleMarkdown)
+		richText := testfile.Read(be.Relaxed(t), path+"/rich.html")
+		be.Debug(t, func() {
+			if richText != dbDoc.RichText {
+				testfile.Write(t, path+"/rich-bad.html", dbDoc.RichText)
+			}
+		})
+		be.Equal(t, richText, dbDoc.RichText)
+
+		md := testfile.Read(be.Relaxed(t), path+"/article.md")
+		be.Debug(t, func() {
+			testfile.Write(t, path+"/article-bad.md", dbDoc.ArticleMarkdown)
+		})
+		be.Equal(t, md, dbDoc.ArticleMarkdown)
 	})
-	be.Equal(t, md, dbDoc.ArticleMarkdown)
 }
