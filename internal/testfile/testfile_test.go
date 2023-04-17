0c56d259177691aac7b25f524f4d8ab9dfbdd167
Carl Johnson <me@carlmjohnson.net>
2023-04-17 13:24:43 -0400

    testfile: Add JSON

---
+++ internal/testfile/testfile_test.go
@@ -0,0 +1,24 @@
+package testfile_test
+
+import (
+	"testing"
+
+	"github.com/spotlightpa/almanack/internal/testfile"
+)
+
+func TestJSON(t *testing.T) {
+	testfile.GlobRun(t, "testdata/*.json", func(path string, t *testing.T) {
+		testfile.JSON(t, path, struct {
+			Data any `json:"data"`
+		}{
+			Data: []struct {
+				Field string `json:"field"`
+				Value int    `json:"value"`
+			}{
+				{"foo", 1},
+				{"bar", 2},
+			},
+		})
+	})
+
+}
