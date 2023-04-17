a0eec7637567f7812b9f29d4f00dd677ed052499
Carl Johnson <me@carlmjohnson.net>
2023-04-17 14:33:33 -0400

    BE: Update for testfile changes

--- internal/blocko/blocko_test.go
+++ internal/blocko/blocko_test.go
@@ -12,18 +12,11 @@ import (
 func TestGoldenFiles(t *testing.T) {
 	testfile.GlobRun(t, "testdata/*.html", func(path string, t *testing.T) {
 		bareName := strings.TrimSuffix(path, ".html")
-		wantMD := bareName + ".md"
-		want := testfile.Read(be.Relaxed(t), wantMD)
-
 		in := testfile.Read(t, path)
+
 		got, err := blocko.HTMLToMarkdown(in)
 		be.NilErr(t, err)
 
-		be.Debug(t, func() {
-			badname := bareName + ".xxx.md"
-			testfile.Write(t, badname, got)
-		})
-
-		be.Equal(t, want, got)
+		testfile.Equal(t, bareName+".md", got)
 	})
 }
