d7e8a946b7a6ffc814f8e5da831b4d89bf662949
Carl Johnson <me@carlmjohnson.net>
2023-04-24 16:50:18 -0400

    ViewSharedArticle: Work with GDocs

--- src/components/ViewSharedArticleAdmin.vue
+++ src/components/ViewSharedArticleAdmin.vue
@@ -8,6 +8,7 @@ import {
   get,
   post,
   postPageCreate,
+  listImages,
   getSharedArticle,
   postSharedArticle,
   postSharedArticleFromArc,
@@ -85,7 +86,7 @@ const emailBody = computed(() => {
     embargoText,
     noteText,
     `Budget:`,
-    a.budget,
+    budget.value,
     a.arc
       ? `Word count: ${intcomma(a.arc.actualWordCount)}
 Lines: ${intcomma(a.arc.actualLineCount)}
@@ -119,22 +120,21 @@ function refreshArc() {
 const { exec: saveExec, apiStateRefs: saveState } = makeState();
 const { isLoadingThrottled: saveLoading, error: saveError } = saveState;
 async function save() {
-  let av = article.value;
   const obj = {
     id: +props.id,
     note: note.value,
     embargo_until: embargo.value,
     status: status.value,
-    publication_date: av.publicationDate,
-    internal_id: av.internalID,
-    byline: av.byline,
-    budget: av.budget,
-    hed: av.hed,
-    description: av.description,
-    lede_image: av.ledeImage,
-    lede_image_credit: av.ledeImageCredit,
-    lede_image_description: av.ledeImageDescription,
-    lede_image_caption: av.ledeImageCaption,
+    publication_date: publicationDate.value,
+    internal_id: internalID.value,
+    byline: byline.value,
+    budget: budget.value,
+    hed: hed.value,
+    description: description.value,
+    lede_image: ledeImage.value,
+    lede_image_credit: ledeImageCredit.value,
+    lede_image_description: ledeImageDescription.value,
+    lede_image_caption: ledeImageCaption.value,
   };
   await saveExec(() => post(postSharedArticle, obj));
   await fetch();
@@ -153,6 +153,18 @@ async function toggleComposer() {
   }
   await nextTick();
   composer.value.$el.scrollIntoView({ behavior: "smooth", block: "center" });
+}
+
+const { computedList: imageList } = watchAPI(
+  () => 0,
+  (page) => get(listImages, { page })
+);
+const images = imageList("images", (obj) => obj);
+function setImageProps(image) {
+  ledeImage.value = image.path;
+  ledeImageDescription.value = image.description;
+  ledeImageCredit.value = image.credit;
+  ledeImageCaption.value = "";
 }
 </script>
 
@@ -175,203 +187,351 @@ async function toggleComposer() {
     <SpinnerProgress :is-loading="apiState.isLoading.value" />
     <ErrorReloader :error="apiState.error.value" @reload="fetch" />
 
-    <article v-if="article && !article.isProcessing" class="message is-primary">
+    <article v-if="article">
       <MetaHead>
         <title>{{ article.internalID }} Admin • Spotlight PA</title>
       </MetaHead>
 
-      <div class="message-header">
-        <p>
-          <font-awesome-icon :icon="['far', 'newspaper']" />
-          {{ article.internalID }}
-        </p>
-        <span class="tags">
-          <router-link v-if="article.id" class="tag" :to="article.detailsRoute">
-            <span class="icon">
-              <font-awesome-icon :icon="['fas', 'file-invoice']" />
-            </span>
-            <span>Partner view</span>
-          </router-link>
-          <a
-            v-if="article.isArc"
-            class="tag"
-            :href="article.arc.arcURL"
-            target="_blank"
-          >
-            <span class="icon">
-              <font-awesome-icon :icon="['fas', 'link']" />
-            </span>
-            <span>Arc</span>
-          </a>
-          <router-link
-            v-if="article.pageRoute"
-            class="tag is-light"
-            :to="article.pageRoute"
-          >
-            <span class="icon">
-              <font-awesome-icon :icon="['fas', 'user-clock']" />
-            </span>
-            <span>Spotlight admin</span>
-          </router-link>
-        </span>
+      <div
+        v-if="article.isGDoc && article.gdocs.warnings.length"
+        class="message is-warning"
+      >
+        <div class="message-header">
+          <span>
+            <font-awesome-icon :icon="['fas', 'circle-exclamation']" />
+
+            <span
+              class="ml-1"
+              v-text="
+                article.gdocs.warnings.length === 1 ? 'Warning' : 'Warnings'
+              "
+            />
+          </span>
+        </div>
+
+        <div class="message-body">
+          <li v-for="(w, i) of article.gdocs.warnings" :key="i">
+            <p v-text="w"></p>
+          </li>
+        </div>
       </div>
-      <div class="message-body">
-        <p class="label">Budget</p>
-        <p class="mb-5 content">{{ article.budget }}</p>
-        <div v-if="!article.pageID" class="mb-5">
-          <div class="label">Import to Spotlight PA</div>
+
+      <div class="message is-primary">
+        <div class="message-header">
+          <p>
+            <font-awesome-icon :icon="['far', 'newspaper']" />
+            {{ article.internalID }}
+          </p>
+          <span class="tags">
+            <TagLink :to="article.detailsRoute" :icon="['fas', 'file-invoice']">
+              Partner view
+            </TagLink>
+            <TagLink
+              v-if="article.isArc"
+              :href="article.arc.arcURL"
+              :icon="['fas', 'link']"
+            >
+              Arc
+            </TagLink>
+            <TagLink
+              v-if="article.isGDoc"
+              :href="article.gdocsURL"
+              :icon="['fas', 'link']"
+            >
+              Google Docs
+            </TagLink>
+            <TagLink
+              v-if="article.pageRoute"
+              :to="article.pageRoute"
+              :icon="['fas', 'user-clock']"
+            >
+              Spotlight admin
+            </TagLink>
+          </span>
+        </div>
+        <div class="message-body">
+          <template v-if="article.isGDoc">
+            <BulmaFieldInput
+              label="Internal ID"
+              :model-value="internalID"
+              help=""
+              @update:modelValue="
+                isDirty = true;
+                internalID = $event;
+              "
+            />
+
+            <BulmaDateTime
+              :model-value="publicationDate"
+              label="Planned Publication Date"
+              help="List when the article should be published."
+              @update:modelValue="
+                publicationDate = $event;
+                isDirty = true;
+              "
+            >
+              Set to
+              <a
+                @click="
+                  publicationDate = new Date();
+                  isDirty = true;
+                "
+                >now</a
+              >. Set to
+
+              <a
+                @click="
+                  publicationDate = tomorrow();
+                  isDirty = true;
+                "
+                >tomorrow</a
+              >.
+            </BulmaDateTime>
+
+            <BulmaTextarea
+              :model-value="budget"
+              label="Budget"
+              help="Description of the story for partners and editors."
+              @update:modelValue="
+                isDirty = true;
+                budget = $event;
+              "
+            />
+
+            <BulmaFieldInput
+              label="Suggested hed"
+              :model-value="hed"
+              help=""
+              @update:modelValue="
+                isDirty = true;
+                hed = $event;
+              "
+            />
+            <BulmaFieldInput
+              label="Suggested description"
+              :model-value="description"
+              help=""
+              @update:modelValue="
+                isDirty = true;
+                description = $event;
+              "
+            />
+            <BulmaFieldInput
+              label="Byline"
+              :model-value="byline"
+              help=""
+              @update:modelValue="
+                isDirty = true;
+                byline = $event;
+              "
+            />
+
+            <BulmaField label="Lede Image" v-slot="{ idForLabel }">
+              <div class="is-flex">
+                <input
+                  :id="idForLabel"
+                  :value="ledeImage"
+                  class="input"
+                  @change="
+                    isDirty = true;
+                    ledeImage = $event.target.value;
+                  "
+                />
+                <BulmaPaste
+                  @paste="
+                    isDirty = true;
+                    ledeImage = $event;
+                  "
+                />
+              </div>
+            </BulmaField>
+
+            <PickerImages
+              :images="images"
+              @select-image="setImageProps($event)"
+            />
+
+            <BulmaTextarea
+              :model-value="ledeImageDescription"
+              label="Lede image description (“alt” text)"
+              help="A description of the image for visually impaired readers"
+              @update:modelValue="
+                isDirty = true;
+                ledeImageDescription = $event;
+              "
+            />
+
+            <BulmaFieldInput
+              :model-value="ledeImageCredit"
+              label="Lede image credit"
+              @update:modelValue="
+                isDirty = true;
+                ledeImageCredit = $event;
+              "
+            />
+
+            <BulmaTextarea
+              :model-value="ledeImageCaption"
+              label="Lede image caption"
+              help="An optional caption underneath the image"
+              @update:modelValue="
+                isDirty = true;
+                ledeImageCaption = $event;
+              "
+            />
+          </template>
+
+          <p v-if="article.isArc" class="label">Budget</p>
+          <p v-if="article.isArc" class="mb-5 content">{{ article.budget }}</p>
+
+          <div v-if="!article.pageID" class="mb-5">
+            <div class="label">Import to Spotlight PA</div>
+            <div class="buttons">
+              <button
+                class="button is-primary has-text-weight-semibold"
+                :class="pageLoading ? 'is-loading' : ''"
+                @click="createPage('news')"
+              >
+                As News article
+              </button>
+              <button
+                class="button is-primary has-text-weight-semibold"
+                :class="pageLoading ? 'is-loading' : ''"
+                @click="createPage('statecollege')"
+              >
+                As State College article
+              </button>
+            </div>
+          </div>
+
+          <ErrorSimple :error="pageError" />
+
+          <h3 class="label">Sharing status</h3>
           <div class="buttons">
             <button
-              class="button is-primary has-text-weight-semibold"
-              :class="pageLoading ? 'is-loading' : ''"
-              @click="createPage('news')"
+              class="button is-small has-text-weight-semibold"
+              :class="statusClass('U')"
+              type="button"
+              @click="
+                status = 'U';
+                isDirty = true;
+              "
             >
-              As News article
+              Not Shared
             </button>
             <button
-              class="button is-primary has-text-weight-semibold"
-              :class="pageLoading ? 'is-loading' : ''"
-              @click="createPage('statecollege')"
+              class="button is-small has-text-weight-semibold"
+              :class="statusClass('S')"
+              type="button"
+              @click="
+                status = 'S';
+                isDirty = true;
+              "
             >
-              As State College article
+              Shared with partners
             </button>
           </div>
-        </div>
 
-        <ErrorSimple :error="pageError" />
+          <div class="mb-5">
+            <BulmaDateTime
+              :model-value="embargo"
+              label="Embargo time"
+              help="List the latest time that an article will be under embargo for partners."
+              @update:modelValue="
+                embargo = $event;
+                isDirty = true;
+              "
+            />
+            <a
+              @click="
+                embargo = tomorrow();
+                isDirty = true;
+              "
+            >
+              Set for tomorrow
+            </a>
+          </div>
 
-        <h3 class="label">Status</h3>
-        <div class="buttons">
-          <button
-            class="button is-small has-text-weight-semibold"
-            :class="statusClass('U')"
-            type="button"
-            @click="
-              status = 'U';
-              isDirty = true;
-            "
-          >
-            Not Shared
-          </button>
-          <button
-            class="button is-small has-text-weight-semibold"
-            :class="statusClass('S')"
-            type="button"
-            @click="
-              status = 'S';
-              isDirty = true;
-            "
-          >
-            Shared with partners
-          </button>
-        </div>
-
-        <div class="mb-5">
-          <BulmaDateTime
-            :model-value="embargo"
-            label="Embargo time"
-            help="List the latest time that an article will be under embargo for partners."
+          <BulmaTextarea
+            :model-value="note"
+            label="Note"
+            help="Additional clarifications and instructions for partners"
             @update:modelValue="
-              embargo = $event;
               isDirty = true;
+              note = $event;
             "
           />
-          <a
-            @click="
-              embargo = tomorrow();
-              isDirty = true;
-            "
-          >
-            Set for tomorrow
-          </a>
-        </div>
 
-        <BulmaTextarea
-          :model-value="note"
-          label="Note"
-          help="Additional clarifications and instructions for partners"
-          @update:modelValue="
-            isDirty = true;
-            note = $event;
-          "
-        />
-
-        <button
-          class="button is-warning has-text-weight-semibold"
-          type="button"
-          :class="arcLoading && 'is-loading'"
-          @click="refreshArc"
-        >
-          Refresh from Arc
-        </button>
-        <ErrorSimple :error="saveError || arcError" class="mt-1" />
-
-        <div class="mt-5 buttons">
           <button
-            class="button is-success has-text-weight-semibold"
-            :class="saveLoading ? 'is-loading' : ''"
-            :disabled="!savingEnabled || null"
-            @click="save()"
+            v-if="article.isArc"
+            class="button is-warning has-text-weight-semibold"
+            type="button"
+            :class="arcLoading && 'is-loading'"
+            @click="refreshArc"
           >
-            Save changes
+            Refresh from Arc
           </button>
-          <button
-            class="button is-danger has-text-weight-semibold"
-            :class="saveLoading ? 'is-loading' : ''"
-            :disabled="!savingEnabled || null"
-            @click="fetch"
-          >
-            Discard changes
-          </button>
-        </div>
-        <button
-          type="button"
-          class="button is-small has-text-weight-semibold"
-          :class="showComposer ? 'is-danger' : 'is-primary'"
-          @click="toggleComposer()"
-        >
-          <span class="icon">
-            <font-awesome-icon :icon="['fas', 'paper-plane']" />
-          </span>
-          <span
-            v-text="!showComposer ? 'Compose Message' : 'Discard Message'"
-          />
-        </button>
+          <ErrorSimple :error="saveError || arcError" class="mt-1" />
 
-        <EmailComposer
-          v-if="showComposer"
-          ref="composer"
-          class="mt-5"
-          :initial-subject="`New Spotlight PA story ${article.internalID}`"
-          :initial-body="emailBody"
-          @hide="showComposer = false"
-        />
-
-        <div
-          v-if="showComposer && article._status !== 'S'"
-          class="mt-5 message is-danger"
-        >
-          <p class="message-body">
-            <strong>Warning:</strong> Article has not been shared with partners
-            yet.
-          </p>
-        </div>
-      </div>
-    </article>
-
-    <article v-if="article && article.isProcessing" class="">
-      <div class="message is-warning">
-        <div class="message-body">
-          <p>Article contents are being processed.</p>
-          <div class="mt-5">
+          <div class="mt-5 buttons">
             <button
-              class="button is-warning has-text-weight-semibold"
-              type="button"
+              class="button is-success has-text-weight-semibold"
+              :class="saveLoading ? 'is-loading' : ''"
+              :disabled="!savingEnabled || null"
+              @click="save()"
+            >
+              Save changes
+            </button>
+            <button
+              class="button is-danger has-text-weight-semibold"
+              :class="saveLoading ? 'is-loading' : ''"
+              :disabled="!savingEnabled || null"
               @click="fetch"
             >
-              Reload
+              Discard changes
             </button>
           </div>
+          <button
+            type="button"
+            class="button is-small has-text-weight-semibold"
+            :class="showComposer ? 'is-danger' : 'is-primary'"
+            @click="toggleComposer()"
+          >
+            <span class="icon">
+              <font-awesome-icon :icon="['fas', 'paper-plane']" />
+            </span>
+            <span
+              v-text="!showComposer ? 'Compose Message' : 'Discard Message'"
+            />
+          </button>
+
+          <EmailComposer
+            v-if="showComposer"
+            ref="composer"
+            class="mt-5"
+            :initial-subject="`New Spotlight PA story ${article.internalID}`"
+            :initial-body="emailBody"
+            @hide="showComposer = false"
+          />
+
+          <div
+            v-if="showComposer && article._status !== 'S'"
+            class="mt-5 message is-danger"
+          >
+            <p class="message-body">
+              <strong>Warning:</strong> Article has not been shared with
+              partners yet.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div v-if="article.isGDoc">
+        <h2 class="title is-5">Article Preview</h2>
+        <div class="textarea" rows="whatever">
+          <h1 class="title">{{ hed }}</h1>
+          <h2 class="subtitle is-3">{{ description }}</h2>
+          <h2 v-if="byline" class="subtitle is-5">By {{ byline }}</h2>
+          <div class="content" v-html="article.gdocs.rich_text" />
         </div>
       </div>
     </article>
