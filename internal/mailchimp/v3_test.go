6538262592bf8409115bbb18c9eb2f213f4a4187
Carl Johnson <me@carlmjohnson.net>
2023-05-05 09:49:12 -0400

    mailchimp.TestV3: Use testfile

--- internal/mailchimp/v3_test.go
+++ internal/mailchimp/v3_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/carlmjohnson/be"
 	"github.com/carlmjohnson/requests"
 	"github.com/spotlightpa/almanack/internal/mailchimp"
+	"github.com/spotlightpa/almanack/internal/testfile"
 )
 
 func TestV3(t *testing.T) {
@@ -18,8 +19,8 @@ func TestV3(t *testing.T) {
 	apiKey := os.Getenv("ALMANACK_MC_TEST_API_KEY")
 	listID := os.Getenv("ALMANACK_MC_TEST_LISTID")
 
-	if os.Getenv("ALMANACK_MC_TEST_RECORD_REQUEST") != "" {
-		cl.Transport = requests.Record(nil, "testdata")
+	if os.Getenv("RECORD") != "" {
+		cl.Transport = requests.Caching(nil, "testdata")
 	}
 	v3 := mailchimp.NewV3(apiKey, listID, &cl)
 	res, err := v3.ListCampaigns(context.Background())
@@ -39,10 +40,5 @@ func TestV3(t *testing.T) {
 	camp := res.Campaigns[0]
 	body, err := mailchimp.ImportPage(context.Background(), &cl, camp.ArchiveURL)
 	be.NilErr(t, err)
-	expect, err := os.ReadFile("testdata/body.html")
-	be.NilErr(t, err)
-	be.Debug(t, func() {
-		os.WriteFile("testdata/got.html", []byte(body), 0644)
-	})
-	be.Equal(t, string(expect), body)
+	testfile.Equal(t, "testdata/body.html", body)
 }
