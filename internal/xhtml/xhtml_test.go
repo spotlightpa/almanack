8d7bdf81c655e14254c222f79e52b8c26e11e93e
Carl Johnson <me@carlmjohnson.net>
2023-05-16 16:49:09 -0400

    xhtml: Add SetInnerHTML

--- internal/xhtml/xhtml_test.go
+++ internal/xhtml/xhtml_test.go
@@ -40,3 +40,20 @@ func TestIsEmpty(t *testing.T) {
 		})
 	}
 }
+
+func TestSetInnerHTML(t *testing.T) {
+	n := xhtml.New("p")
+	be.NilErr(t, xhtml.SetInnerHTML(n, "Hello, <i>World!</i>"))
+	be.Equal(t, `<p>Hello, <i>World!</i></p>`, xhtml.ToString(n))
+
+	be.NilErr(t, xhtml.SetInnerHTML(n, "Jello, <i>World!</i>"))
+	be.Equal(t, `<p>Jello, <i>World!</i></p>`, xhtml.ToString(n))
+
+	n = xhtml.New("script")
+	be.NilErr(t, xhtml.SetInnerHTML(n, "let i = 1 > 2"))
+	be.Equal(t, `<script>let i = 1 > 2</script>`, xhtml.ToString(n))
+
+	n = xhtml.New("p")
+	be.NilErr(t, xhtml.SetInnerHTML(n, "</a></b>"))
+	be.Equal(t, `<p></p>`, xhtml.ToString(n))
+}
