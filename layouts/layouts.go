614141dc6260f01296e49c3fe5d969e675f44bcb
Carl Johnson <me@carlmjohnson.net>
2023-06-15 08:46:01 -0400

    Remove more embed

--- layouts/layouts.go
+++ layouts/layouts.go
@@ -1,19 +1,11 @@
 package layouts
 
 import (
-	"embed"
 	"html/template"
 )
 
-var FS embed.FS
-
 func makeTemplate(names ...string) *template.Template {
-	baseName := names[0]
-	return template.Must(
-		template.
-			New(baseName).
-			Funcs(nil).
-			ParseFS(FS, names...))
+	return nil
 }
 
 var MailChimp = makeTemplate("mailchimp.html")
