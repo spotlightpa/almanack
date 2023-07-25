c3bdf65b2fd104993d340b153e6135d2eae6b8c4
Carl Johnson <me@carlmjohnson.net>
2023-07-25 10:46:59 -0400

    xhtml: Don't bother pooling

--- internal/xhtml/string.go
+++ internal/xhtml/string.go
@@ -3,45 +3,34 @@ package xhtml
 import (
 	"bytes"
 	"strings"
-	"sync"
 
 	"golang.org/x/net/html"
 )
 
 func ToBuffer(n *html.Node) *bytes.Buffer {
 	var buf bytes.Buffer
+	buf.Grow(4 * 1 >> 10)
 	if err := html.Render(&buf, n); err != nil {
 		panic(err)
 	}
 	return &buf
 }
 
-var pool sync.Pool
-
-func poolGet() *bytes.Buffer {
-	if v := pool.Get(); v != nil {
-		buf := v.(*bytes.Buffer)
-		buf.Reset()
-		return buf
-	}
-	return &bytes.Buffer{}
-}
-
 func ToString(n *html.Node) string {
-	buf := poolGet()
-	defer pool.Put(buf)
-	if err := html.Render(buf, n); err != nil {
+	var buf strings.Builder
+	buf.Grow(256)
+	if err := html.Render(&buf, n); err != nil {
 		panic(err)
 	}
 	return buf.String()
 }
 
 func ContentsToString(n *html.Node) string {
-	buf := poolGet()
-	defer pool.Put(buf)
+	var buf strings.Builder
+	buf.Grow(256)
 
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		if err := html.Render(buf, c); err != nil {
+		if err := html.Render(&buf, c); err != nil {
 			panic(err)
 		}
 	}
@@ -51,11 +40,11 @@ func ContentsToString(n *html.Node) string {
 // InnerBlocksToString is the same as ContentsToString,
 // but it separates top level nodes with a line break.
 func InnerBlocksToString(n *html.Node) string {
-	buf := poolGet()
-	defer pool.Put(buf)
+	var buf strings.Builder
+	buf.Grow(256)
 
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		if err := html.Render(buf, c); err != nil {
+		if err := html.Render(&buf, c); err != nil {
 			panic(err)
 		}
 		buf.WriteString("\n")
@@ -65,8 +54,8 @@ func InnerBlocksToString(n *html.Node) string {
 
 // InnerText joins and trims the text node children of n.
 func InnerText(n *html.Node) string {
-	buf := poolGet()
-	defer pool.Put(buf)
+	var buf strings.Builder
+	buf.Grow(256)
 
 	VisitAll(n, func(n *html.Node) {
 		if n.Type == html.TextNode {
