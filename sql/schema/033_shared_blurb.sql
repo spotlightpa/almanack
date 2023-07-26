b34f889f89986f3ba8bf9ef56ad1512bfee6c4af
Carl Johnson <me@carlmjohnson.net>
2023-07-26 15:34:31 -0400

    SQL: Fix shared_blurb migration

--- sql/schema/033_shared_blurb.sql
+++ sql/schema/033_shared_blurb.sql
@@ -4,12 +4,17 @@ ALTER TABLE "shared_article"
 ALTER TABLE "shared_article" DISABLE TRIGGER row_updated_at_on_shared_article_trigger_;
 
 UPDATE
-  "shared_article"
+  "shared_article" AS sa
 SET
-  "blurb" = "description";
+  blurb = gd."metadata" ->> 'blurb'
+FROM
+  "g_docs_doc" gd
+WHERE
+  sa."source_type" = 'gdocs'
+  AND cast(sa."raw_data" AS bigint) = gd.id;
 
 ALTER TABLE shared_article ENABLE TRIGGER row_updated_at_on_shared_article_trigger_;
 
 ---- create above / drop below ----
 ALTER TABLE "shared_article"
-  DROP COLUMN "description";
+  DROP COLUMN "blurb";
