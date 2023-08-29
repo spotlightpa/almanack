31fdab0eac6432c54ddb6e05ad916fdcd1c3a1fe
Carl Johnson <me@carlmjohnson.net>
2023-08-29 10:12:18 -0400

    TestInnerText: Use a table

--- internal/xhtml/string_test.go
+++ internal/xhtml/string_test.go
@@ -11,11 +11,19 @@ import (
 )
 
 func TestInnerText(t *testing.T) {
-	s := `<p><em></em>&lt;div class=&#34;flourish-embed flourish-cards&#34; data-src=&#34;visualisation/14836391&#34;&gt;&lt;script src=&#34;<a href="https://public.flourish.studio/resources/embed.js">https://public.flourish.studio/resources/embed.js</a>&#34;&gt;&lt;/script&gt;&lt;/div&gt;
-	</p>`
-	doc, err := html.Parse(strings.NewReader(s))
-	be.NilErr(t, err)
-	p := xhtml.Find(doc, xhtml.WithAtom(atom.P))
-	got := xhtml.InnerText(p)
-	be.Equal(t, `<div class="flourish-embed flourish-cards" data-src="visualisation/14836391"><script src="https://public.flourish.studio/resources/embed.js"></script></div>`, got)
+	for _, tc := range []struct {
+		input, want string
+	}{
+		{
+			`<p><em></em>&lt;div class=&#34;flourish-embed flourish-cards&#34; data-src=&#34;visualisation/14836391&#34;&gt;&lt;script src=&#34;<a href="https://public.flourish.studio/resources/embed.js">https://public.flourish.studio/resources/embed.js</a>&#34;&gt;&lt;/script&gt;&lt;/div&gt;
+	</p>`,
+			`<div class="flourish-embed flourish-cards" data-src="visualisation/14836391"><script src="https://public.flourish.studio/resources/embed.js"></script></div>`,
+		},
+	} {
+		doc, err := html.Parse(strings.NewReader(tc.input))
+		be.NilErr(t, err)
+		p := xhtml.Find(doc, xhtml.WithAtom(atom.P))
+		got := xhtml.InnerText(p)
+		be.Equal(t, tc.want, got)
+	}
 }
