fc890e8d6d5162106716c8a6ff36294acf76af51
Carl Johnson <me@carlmjohnson.net>
2023-09-13 16:08:28 -0400

    Upgrade muxpatterns (#325)

--- internal/httpx/path.go
+++ internal/httpx/path.go
@@ -1,7 +1,6 @@
 package httpx
 
 import (
-	"context"
 	"net/http"
 
 	"github.com/jba/muxpatterns"
@@ -13,18 +12,7 @@ type muxValue struct {
 	*http.Request
 }
 
-func WithPathValue(mux *muxpatterns.ServeMux) Middleware {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := r.Context()
-			ctx = context.WithValue(ctx, muxKey{}, muxValue{mux, r})
-			r2 := r.WithContext(ctx)
-			next.ServeHTTP(w, r2)
-		})
-	}
-}
-
+// TODO: replace with stdlib after https://github.com/golang/go/issues/61410
 func PathValue(r *http.Request, name string) string {
-	muxval, _ := r.Context().Value(muxKey{}).(muxValue)
-	return muxval.PathValue(muxval.Request, name)
+	return muxpatterns.PathValue(r, name)
 }
