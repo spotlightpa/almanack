5f85355ab0d599ca41e239568c6db5f77402fd44
Carl Johnson <me@carlmjohnson.net>
2023-09-26 11:59:29 -0400

    httpx: Remove dead code

--- internal/httpx/path.go
+++ internal/httpx/path.go
@@ -6,12 +6,6 @@ import (
 	"github.com/jba/muxpatterns"
 )
 
-type muxKey struct{}
-type muxValue struct {
-	*muxpatterns.ServeMux
-	*http.Request
-}
-
 // TODO: replace with stdlib after https://github.com/golang/go/issues/61410
 func PathValue(r *http.Request, name string) string {
 	return muxpatterns.PathValue(r, name)
