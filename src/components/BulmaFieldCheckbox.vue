5c88ffcc97f69eb047c7866a7bd103f74f274f56
Carlana Johnson <mail@carlana.net>
2024-01-10 11:27:16 -0500

    Add BulmaFieldCheckbox

---
+++ src/components/BulmaFieldCheckbox.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  props: {
+    label: String,
+    labelClass: {
+      type: String,
+      default: "label",
+    },
+    modelValue: Boolean,
+    placeholder: String,
+    help: String,
+    name: String,
+  },
+  computed: {
+    fieldProps() {
+      return {
+        label: this.label,
+        help: this.help,
+        labelClass: this.labelClass,
+      };
+    },
+    checkboxValue: {
+      get() {
+        return this.modelValue;
+      },
+      set(event) {
+        this.$emit("update:modelValue", event);
+      },
+    },
+  },
+};
+</script>
+
+<template>
+  <BulmaField v-slot="{ idForLabel }" v-bind="fieldProps">
+    <div>
+      <label class="checkbox">
+        <input :id="idForLabel" v-model="checkboxValue" type="checkbox" />
+        <slot />
+      </label>
+    </div>
+  </BulmaField>
+</template>
