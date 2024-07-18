6f94f5b1950585e8fbbc3170e23f7531b4cabaa9
Carlana Johnson <mail@carlana.net>
2024-07-17 21:22:59 -0400

    processDocHTML: Simplify use of data tags a little

--- pkg/almanack/service-gdocs-raw.go
+++ pkg/almanack/service-gdocs-raw.go
@@ -4,40 +4,47 @@ import (
 	"fmt"
 
 	"github.com/spotlightpa/almanack/internal/db"
+	"github.com/spotlightpa/almanack/internal/iterx"
 	"github.com/spotlightpa/almanack/internal/xhtml"
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
 )
 
-func fixRawHTMLPlaceholders(rawHTML *html.Node) {
-	embeds := xhtml.SelectSlice(rawHTML, xhtml.WithAtom(atom.Data))
-	for _, dataEl := range embeds {
-		embed := extractDataTag(dataEl)
-		switch embed.Type {
-		default:
-			panic("unknown embed type: " + embed.Type)
-		case dtSpotlightRaw, dtSpotlightText:
-			dataEl.Parent.RemoveChild(dataEl)
-		case dtPartnerText:
+func fixRawHTMLPlaceholders(doc *html.Node) {
+	// Remove Spotlight PA exclusives
+	for dataEl, _ := range iterx.Concat2(
+		dataEls(doc, dtSpotlightText),
+		dataEls(doc, dtSpotlightRaw),
+	) {
+		dataEl.Parent.RemoveChild(dataEl)
+	}
+	// Include partner text as is
+	for dataEl, text := range dataEls(doc, dtPartnerText) {
+		xhtml.ReplaceWith(dataEl, &html.Node{
+			Type: html.RawNode,
+			Data: text,
+		})
+	}
+	for dataEl, value := range dataEls(doc, dtDBEmbed) {
+		dbembed := dbEmbedFromString(value)
+		switch dbembed.Type {
+		// Replace images with red placeholder text
+		case db.ImageEmbedTag:
+			placeholder := xhtml.New("h2", "style", "color: red;")
+			xhtml.AppendText(placeholder, fmt.Sprintf("Embed #%d", dbembed.N))
+			xhtml.ReplaceWith(dataEl, placeholder)
+
+		// Include other embeds as is
+		case db.RawEmbedTag, db.ToCEmbedTag, db.PartnerRawEmbedTag:
 			xhtml.ReplaceWith(dataEl, &html.Node{
 				Type: html.RawNode,
-				Data: embed.Value,
+				Data: dbembed.Value.(string),
 			})
-		case dtDBEmbed:
-			dbembed := extractDBEmbed(embed)
-			switch dbembed.Type {
-			case db.RawEmbedTag, db.ToCEmbedTag, db.PartnerRawEmbedTag:
-				xhtml.ReplaceWith(dataEl, &html.Node{
-					Type: html.RawNode,
-					Data: dbembed.Value.(string),
-				})
-			case db.ImageEmbedTag:
-				placeholder := xhtml.New("h2", "style", "color: red;")
-				xhtml.AppendText(placeholder, fmt.Sprintf("Embed #%d", dbembed.N))
-				xhtml.ReplaceWith(dataEl, placeholder)
-			default:
-				panic("unknown embed type: " + dbembed.Type)
-			}
+		default:
+			panic("unknown embed type: " + dbembed.Type)
 		}
+	}
+	if el := xhtml.Select(doc, xhtml.WithAtom(atom.Data)); el != nil {
+		panic("unprocessed data element: " + xhtml.OuterHTML(el))
 	}
 }
