59570313d64d2b4e4d2d77fa58f2e5eb3bb52397
Carlana Johnson <mail@carlana.net>
2024-08-08 17:12:47 -0400

    requests: Block use of http.DefaultClient

--- pkg/almlog/httplogger.go
+++ pkg/almlog/httplogger.go
@@ -1,14 +1,19 @@
 package almlog
 
 import (
+	"errors"
 	"net/http"
 	"time"
 
 	"github.com/carlmjohnson/requests"
 )
 
+var HTTPTransport http.RoundTripper
+
 func init() {
-	http.DefaultTransport = requests.LogTransport(http.DefaultTransport, logReq)
+	HTTPTransport = requests.LogTransport(http.DefaultTransport, logReq)
+	http.DefaultTransport = requests.ErrorTransport(errors.New("use of http.DefaultTransport"))
+	http.DefaultClient.Transport = requests.ErrorTransport(errors.New("use of http.DefaultClient"))
 }
 
 func logReq(req *http.Request, res *http.Response, err error, duration time.Duration) {
