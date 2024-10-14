4761200ce9da01136854c236c9055d68eafe311c
Carlana Johnson <mail@carlana.net>
2024-10-14 09:48:38 -0400

    iterx: Add Concat

--- internal/iterx/iterx.go
+++ internal/iterx/iterx.go
@@ -18,21 +18,8 @@ func Filter[T any](seq iter.Seq[T], match func(T) bool) iter.Seq[T] {
 
 // First returns the first item in a sequence or the zero value.
 func First[T any](seq iter.Seq[T]) (v T) {
-	for v := range seq {
+	for v = range seq {
 		return v
 	}
 	return
-}
-
-// Concat2 streams each sequence it was passed in order.
-func Concat2[T1, T2 any](seqs ...iter.Seq2[T1, T2]) iter.Seq2[T1, T2] {
-	return func(yield func(T1, T2) bool) {
-		for _, seq := range seqs {
-			for v1, v2 := range seq {
-				if !yield(v1, v2) {
-					return
-				}
-			}
-		}
-	}
 }
