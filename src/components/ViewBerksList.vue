5afad09bbfb1a2e6ef062e42e156d2e06915dba9
Carlana <mail@carlana.net>
2025-04-25 17:49:15 -0400

    Add ViewBerksList; reorganize links

---
+++ src/components/ViewBerksList.vue
@@ -0,0 +1,43 @@
+<script>
+import { watchAPI } from "@/api/service-util.js";
+import { useClient } from "@/api/client.js";
+import PageListItem from "@/api/spotlightpa-page-list-item.js";
+
+export default {
+  props: { page: { default: "" } },
+  setup(props) {
+    let { listPages } = useClient();
+    const { apiState, fetch, computedList, computedProp } = watchAPI(
+      () => props.page,
+      (page) =>
+        listPages({
+          params: { page, path: "content/berks/" },
+        })
+    );
+
+    return {
+      apiState,
+      fetch,
+      pages: computedList("pages", (page) => new PageListItem(page)),
+      nextPage: computedProp("next_page", (page) => ({
+        name: "statecollege-pages",
+        query: { page },
+      })),
+    };
+  },
+};
+</script>
+
+<template>
+  <MetaHead>
+    <title>State College Articles • Spotlight PA Almanack</title>
+  </MetaHead>
+  <PageList
+    title="State College Articles"
+    :page="page"
+    :next-page="nextPage"
+    :api-state="apiState"
+    :reload="fetch"
+    :pages="pages"
+  ></PageList>
+</template>
