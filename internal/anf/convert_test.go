c9b8ae1b3c17886a094e0dca2e56a860696bfd9f
Carlana <mail@carlana.net>
2025-08-22 11:29:00 -0400

    Init package anf

---
+++ internal/anf/convert_test.go
@@ -0,0 +1,21 @@
+package anf_test
+
+import (
+	"testing"
+
+	"github.com/carlmjohnson/be"
+	"github.com/carlmjohnson/be/testfile"
+	"github.com/spotlightpa/almanack/internal/anf"
+)
+
+func TestConvert(t *testing.T) {
+	testfile.Run(t, "testdata/*/article.html", func(t *testing.T, match string) {
+		in := testfile.Read(t, match)
+		art, err := anf.ConvertToAppleNews([]byte(in))
+		be.NilErr(t, err)
+		art.Metadata.DateCreated = nil
+		art.Metadata.DateModified = nil
+		art.Metadata.DatePublished = nil
+		testfile.EqualJSON(t, testfile.Ext(match, ".json"), art)
+	})
+}
