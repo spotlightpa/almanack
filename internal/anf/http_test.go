4173d39a8ae0165832ea79032b72621240a5b365
Carlana <mail@carlana.net>
2025-08-22 16:54:30 -0400

    Add HHMacTransport

---
+++ internal/anf/http_test.go
@@ -0,0 +1,29 @@
+package anf_test
+
+import (
+	"bufio"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/carlmjohnson/be"
+	"github.com/carlmjohnson/be/testfile"
+	"github.com/spotlightpa/almanack/internal/anf"
+)
+
+func TestHMACSignRequest(t *testing.T) {
+	testfile.Run(t, "testdata/req.*.raw", func(t *testing.T, match string) {
+		in := testfile.Read(t, match)
+		buf := bufio.NewReader(strings.NewReader(in))
+		req, err := http.ReadRequest(buf)
+		be.NilErr(t, err)
+
+		now := time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)
+		be.NilErr(t, anf.HHMACSignRequest(req, "key", "abc123", now))
+		signed, err := httputil.DumpRequest(req, true)
+		be.NilErr(t, err)
+		testfile.Equalish(t, testfile.Ext(match, "signed"), string(signed))
+	})
+}
