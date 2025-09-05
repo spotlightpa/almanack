71172d4d636d7facfafcbf13e7dfd16c7dfdae02
Carlana <mail@carlana.net>
2025-09-05 14:54:42 -0400

    Add anf.FromDB; tweak styles

---
+++ internal/anf/fromdb.go
@@ -0,0 +1,94 @@
+package anf
+
+import (
+	"cmp"
+	"fmt"
+	"strings"
+
+	"github.com/spotlightpa/almanack/internal/db"
+	"golang.org/x/net/html"
+)
+
+func FromDB(item db.AppleNewsFeed) (*Article, error) {
+	comps, err := buildComponents(item.ContentHtml)
+	if err != nil {
+		return nil, err
+	}
+	a := templateDoc.Clone()
+	a.Title = item.Title
+	a.Language = cmp.Or(item.Language, "en-us")
+	a.Metadata = &Metadata{
+		Authors:      item.Authors,
+		CanonicalURL: item.URL,
+		// CoverArt:            ,
+		// Excerpt:             "",
+		// Keywords:            "",
+		ThumbnailURL: item.Image,
+		// TransparentToolbar:  "",
+		// VideoURL:            "",
+		// Links:               "",
+		DateCreated:         &item.ExternalPublishedAt,
+		DateModified:        &item.ExternalUpdatedAt,
+		DatePublished:       &item.ExternalPublishedAt,
+		GeneratorName:       "Spotlight PA Feed2ANF",
+		GeneratorVersion:    "0.0.1",
+		GeneratorIdentifier: "",
+	}
+	cover := FillMode("cover")
+	center := VerticalAlignment("center")
+	a.Components = []Component{
+		TextComponent{
+			Role:      "intro",
+			Layout:    "eyebrowLayout",
+			TextStyle: "eyebrowStyle",
+			Text:      "\u00a0" + item.Category + "\u00a0",
+		},
+		TextComponent{
+			Role:   "title",
+			Text:   item.Title,
+			Layout: "titleLayout",
+		},
+		TextComponent{
+			Role: "author",
+			Text: "by " + item.Author,
+		},
+		TextComponent{
+			Role:   "header",
+			Layout: "headerImageLayout",
+			Style: ComponentStyle{
+				Fill: ImageFill{
+					Type:              "image",
+					URL:               item.Image,
+					FillMode:          &cover,
+					VerticalAlignment: &center,
+				},
+			},
+		},
+		/*
+					{
+			      "role": "header",
+			      "layout": "headerImageLayout",
+			      "style": {
+			        "fill": {
+			          "type": "image",
+			          "URL": "https://developer.apple.com/news-publisher/download/Apple-News-Example-Articles/images/Iceland01.jpg",
+			          "fillMode": "cover",
+			          "verticalAlignment": "center"
+			        }
+			      }
+			    },
+		*/
+	}
+
+	a.Components = append(a.Components, comps...)
+	return &a, nil
+}
+
+func buildComponents(htmlContent string) ([]Component, error) {
+	doc, err := html.Parse(strings.NewReader(htmlContent))
+	if err != nil {
+		return nil, fmt.Errorf("BuildComponents: parsing HTML: %w", err)
+	}
+	art := ConvertHTMLToAppleNews(doc)
+	return art.Components, nil
+}
