4e8c5fd0994cef26b830defb2fe61a3eba7dac02
Carlana <mail@carlana.net>
2025-09-08 09:26:41 -0400

    Renaming

--- internal/feed2anf/doc.go
+++
@@ -1,2 +0,0 @@
-// Package feed2anf has tools for publishing an ANF article from a JSON Feed
-package feed2anf
---
+++ internal/jsonfeed/doc.go
@@ -0,0 +1,2 @@
+// Package jsonfeed has tools for publishing an ANF article from a JSON Feed
+package jsonfeed
