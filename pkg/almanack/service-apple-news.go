3b29ac493566d035ab9f4468eb7aa10704eb81f7
Carlana <mail@carlana.net>
2025-09-08 09:33:02 -0400

    Refactor

--- pkg/almanack/service-apple-news.go
+++ pkg/almanack/service-apple-news.go
@@ -2,12 +2,9 @@ package almanack
 
 import (
 	"context"
-	"encoding/json"
 
 	"github.com/carlmjohnson/errorx"
-	"github.com/carlmjohnson/requests"
 	"github.com/spotlightpa/almanack/internal/anf"
-	"github.com/spotlightpa/almanack/internal/jsonfeed"
 	"github.com/spotlightpa/almanack/pkg/almlog"
 )
 
@@ -15,7 +12,7 @@ func (svc Services) PublishAppleNewsFeed(ctx context.Context) (err error) {
 	defer errorx.Trace(&err)
 	l := almlog.FromContext(ctx)
 
-	if err := svc.UpdateAppleNewsArchive(ctx); err != nil {
+	if err := svc.NewsFeed.UpdateAppleNewsArchive(ctx, svc.Client, svc.Queries); err != nil {
 		return err
 	}
 	// Check for unuploaded items
@@ -39,27 +36,4 @@ func (svc Services) PublishAppleNewsFeed(ctx context.Context) (err error) {
 		}
 	}
 	return nil
-}
-
-func (svc Services) UpdateAppleNewsArchive(ctx context.Context) (err error) {
-	defer errorx.Trace(&err)
-	l := almlog.FromContext(ctx)
-
-	// Fetch the feed
-	var source jsonfeed.Feed
-	if err = requests.
-		URL(svc.NewsFeed.URL).
-		Client(svc.Client).
-		ToJSON(&source).
-		Fetch(ctx); err != nil {
-		return err
-	}
-	// Update feed archives
-	data, err := json.Marshal(source.Items)
-	if err != nil {
-		return err
-	}
-	updated, err := svc.Queries.UpsertNewsFeedArchives(ctx, data)
-	l.InfoContext(ctx, "UpdateAppleNewsArchive", "updated_rows", updated)
-	return err
 }
