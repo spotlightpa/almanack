261926071c5a7179cdae830df3424bdf6e0e5be6
Carlana <mail@carlana.net>
2025-09-09 17:16:32 -0400

    Fix PublishAppleNewsFeed

--- pkg/almanack/service-apple-news.go
+++ pkg/almanack/service-apple-news.go
@@ -2,9 +2,11 @@ package almanack
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/carlmjohnson/errorx"
 	"github.com/spotlightpa/almanack/internal/anf"
+	"github.com/spotlightpa/almanack/internal/db"
 	"github.com/spotlightpa/almanack/pkg/almlog"
 )
 
@@ -29,11 +31,29 @@ func (svc Services) PublishAppleNewsFeed(ctx context.Context) (err error) {
 			return err
 		}
 		// Upload to Apple
-		if err = svc.ANF.Publish(ctx, svc.Client, art); err != nil {
-			return err
+		var res *anf.Response
+		if newItem.AppleID == "" {
+			res, err = svc.ANF.Publish(ctx, art)
+			if err != nil {
+				err = fmt.Errorf("publishing %q to Apple: %w", art.Metadata.CanonicalURL, err)
+				l.ErrorContext(ctx, "error", "error", err)
+				continue
+			}
+		} else {
+			// TODO fetch revision ID
+			res, err = svc.ANF.Update(ctx, art, newItem.AppleID, newItem.AppleRevision)
+			if err != nil {
+				err = fmt.Errorf("updating %q to Apple: %w", art.Metadata.CanonicalURL, err)
+				l.ErrorContext(ctx, "error", "error", err)
+				continue
+			}
 		}
 		// Mark as uploaded
-		_, err = svc.Queries.UpdateFeedUploaded(ctx, newItem.ID)
+		_, err = svc.Queries.UpdateFeedAppleID(ctx, db.UpdateFeedAppleIDParams{
+			ID:            newItem.ID,
+			AppleID:       res.Data.ID,
+			AppleRevision: res.Data.Revision,
+		})
 		if err != nil {
 			return err
 		}
