bc5959320ec087953968ad6f446593ffd447ed32
exe.dev user <exedev@plasma-gopher.exe.xyz>
2026-02-24 19:47:35 +0000

    Convert more View components to script setup
    ViewPagesList, ViewAuthorizedDomains, ViewFileUploader, ViewFrontpageEditor
    
    Co-authored-by: Shelley <shelley@exe.dev>

--- src/components/ViewPagesList.vue
+++ src/components/ViewPagesList.vue
@@ -1,31 +1,26 @@
-<script>
+<script setup>
 import { watchAPI } from "@/api/service-util.js";
 import { get, listPages } from "@/api/client-v2.js";
 import PageListItem from "@/api/spotlightpa-page-list-item.js";
 import { useRoute } from "vue-router";
 
-export default {
-  props: { page: { default: "" } },
-  setup(props) {
-    const route = useRoute();
+const props = defineProps({
+  page: { default: "" },
+});
 
-    const { apiState, fetch, computedList, computedProp } = watchAPI(
-      () => [props.page, route.meta.contentPath],
-      ([page, path]) => get(listPages, { page, path })
-    );
+const route = useRoute();
 
-    return {
-      apiState,
-      fetch,
-      title: route.meta.title,
-      pages: computedList("pages", (page) => new PageListItem(page)),
-      nextPage: computedProp("next_page", (page) => ({
-        name: route.name,
-        query: { page },
-      })),
-    };
-  },
-};
+const { apiState, fetch, computedList, computedProp } = watchAPI(
+  () => [props.page, route.meta.contentPath],
+  ([page, path]) => get(listPages, { page, path })
+);
+
+const title = route.meta.title;
+const pages = computedList("pages", (page) => new PageListItem(page));
+const nextPage = computedProp("next_page", (page) => ({
+  name: route.name,
+  query: { page },
+}));
 </script>
 
 <template>
