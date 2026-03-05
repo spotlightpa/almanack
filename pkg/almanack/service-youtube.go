2e1063764145f53854bf8e5f00a18e19f86f373e
Carlana <mail@carlana.net>
2026-03-05 16:31:47 -0500

    Upload Youtube shorts

--- pkg/almanack/service-youtube.go
+++ pkg/almanack/service-youtube.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/earthboundkid/errorx/v2"
+	"github.com/spotlightpa/almanack/internal/db"
 	"github.com/spotlightpa/almanack/internal/youtube"
 )
 
@@ -17,5 +18,35 @@ func (svc Services) UpdateYouTubeFeed(ctx context.Context) (err error) {
 	if err = youtube.UpdateCache(ctx, *svc.Queries, entries); err != nil {
 		return err
 	}
-	return nil
+	data, err := svc.Queries.ListYouTubeWhereShort(ctx, db.ListYouTubeWhereShortParams{
+		Limit:  20,
+		Offset: 0,
+	})
+	if err != nil {
+		return err
+	}
+	type video struct {
+		Title     string `json:"title"`
+		URL       string `json:"url"`
+		Thumbnail string `json:"thumbnail"`
+	}
+	videos := make([]video, len(data))
+	for i, item := range data {
+		videos[i] = video{
+			Title:     item.Title,
+			URL:       item.URL,
+			Thumbnail: item.ThumbnailUrl,
+		}
+	}
+	return UploadJSON(
+		ctx,
+		svc.FileStore,
+		"feeds/youtube-shorts.json",
+		"public, max-age=300",
+		struct {
+			Videos []video `json:"videos"`
+		}{
+			videos,
+		},
+	)
 }
