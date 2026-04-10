259c8716ae89b32d396ef7289704ef17ba078d7e
Carlana <mail@carlana.net>
2026-04-10 12:32:57 -0400

    router: Chain middleware

--- pkg/api/router.go
+++ pkg/api/router.go
@@ -21,11 +21,12 @@ func (app *appEnv) routes() http.Handler {
 	}
 
 	// Start public endpoints
-	standardMW.Control(mux, `GET /api/arc-image`, app.getArcImage)
-	standardMW.HandleFunc(mux, `GET /api/bookmarklet/{slug}`, app.getBookmarklet)
-	standardMW.HandleFunc(mux, `GET /api/healthcheck`, app.ping)
-	standardMW.HandleFunc(mux, `GET /api/healthcheck/{code}`, app.pingErr)
-	standardMW.HandleFunc(mux, `POST /api/identity-hook`, app.postIdentityHook)
+	standardMW.
+		Control(mux, `GET /api/arc-image`, app.getArcImage).
+		HandleFunc(mux, `GET /api/bookmarklet/{slug}`, app.getBookmarklet).
+		HandleFunc(mux, `GET /api/healthcheck`, app.ping).
+		HandleFunc(mux, `GET /api/healthcheck/{code}`, app.pingErr).
+		HandleFunc(mux, `POST /api/identity-hook`, app.postIdentityHook)
 	// End public endpoints
 
 	authMW := standardMW.With(app.authHeaderMiddleware)
@@ -35,44 +36,46 @@ func (app *appEnv) routes() http.Handler {
 	partnerMW := authMW.With(app.hasRoleMiddleware("editor"))
 
 	// Start partner endpoints
-	partnerMW.Control(mux, `GET /api/shared-article`, app.getSharedArticle)
-	partnerMW.Control(mux, `GET /api/shared-articles`, app.listSharedArticles)
+	partnerMW.
+		Control(mux, `GET /api/shared-article`, app.getSharedArticle).
+		Control(mux, `GET /api/shared-articles`, app.listSharedArticles)
 	// End partner endpoints
 
 	spotlightMW := authMW.With(app.hasRoleMiddleware("Spotlight PA"))
 
 	// Start Spotlight endpoints
-	spotlightMW.HandleFunc(mux, `GET /api/all-series`, app.listAllSeries)
-	spotlightMW.HandleFunc(mux, `GET /api/all-topics`, app.listAllTopics)
-	spotlightMW.HandleFunc(mux, `GET /api/authorized-addresses`, app.listAddresses)
-	spotlightMW.HandleFunc(mux, `POST /api/authorized-addresses`, app.postAddress)
-	spotlightMW.HandleFunc(mux, `GET /api/authorized-domains`, app.listDomains)
-	spotlightMW.HandleFunc(mux, `POST /api/authorized-domains`, app.postDomain)
-	spotlightMW.HandleFunc(mux, `POST /api/create-signed-upload`, app.postSignedUpload)
-	spotlightMW.Control(mux, `POST /api/donor-wall`, app.postDonorWall)
-	spotlightMW.HandleFunc(mux, `POST /api/files-create`, app.postFileCreate)
-	spotlightMW.HandleFunc(mux, `GET /api/files-list`, app.listFiles)
-	spotlightMW.HandleFunc(mux, `POST /api/files-update`, app.postFileUpdate)
-	spotlightMW.HandleFunc(mux, `GET /api/gdocs-doc`, app.getGDocsDoc)
-	spotlightMW.HandleFunc(mux, `POST /api/gdocs-doc`, app.postGDocsDoc)
-	spotlightMW.HandleFunc(mux, `POST /api/image-update`, app.postImageUpdate)
-	spotlightMW.HandleFunc(mux, `GET /api/images`, app.listImages)
-	spotlightMW.HandleFunc(mux, `POST /api/message`, app.postMessage)
-	spotlightMW.HandleFunc(mux, `GET /api/page`, app.getPage)
-	spotlightMW.HandleFunc(mux, `POST /api/page`, app.postPage)
-	spotlightMW.HandleFunc(mux, `POST /api/page-create`, app.postPageCreate)
-	spotlightMW.Control(mux, `POST /api/page-load`, app.postPageLoad)
-	spotlightMW.HandleFunc(mux, `GET /api/pages`, app.listPages)
-	spotlightMW.HandleFunc(mux, `GET /api/pages-by-fts`, app.listPagesByFTS)
-	spotlightMW.HandleFunc(mux, `POST /api/page-refresh`, app.postPageRefresh)
-	spotlightMW.HandleFunc(mux, `POST /api/shared-article`, app.postSharedArticle)
-	spotlightMW.HandleFunc(mux, `POST /api/shared-article-from-gdocs`, app.postSharedArticleFromGDocs)
-	spotlightMW.HandleFunc(mux, `GET /api/sidebar`, app.siteDataGet(almanack.SidebarLoc))
-	spotlightMW.HandleFunc(mux, `POST /api/sidebar`, app.siteDataSet(almanack.SidebarLoc))
-	spotlightMW.Control(mux, `GET /api/site-data`, app.getSiteData)
-	spotlightMW.Control(mux, `POST /api/site-data`, app.postSiteData)
-	spotlightMW.HandleFunc(mux, `GET /api/site-params`, app.siteDataGet(almanack.SiteParamsLoc))
-	spotlightMW.HandleFunc(mux, `POST /api/site-params`, app.siteDataSet(almanack.SiteParamsLoc))
+	spotlightMW.
+		HandleFunc(mux, `GET /api/all-series`, app.listAllSeries).
+		HandleFunc(mux, `GET /api/all-topics`, app.listAllTopics).
+		HandleFunc(mux, `GET /api/authorized-addresses`, app.listAddresses).
+		HandleFunc(mux, `POST /api/authorized-addresses`, app.postAddress).
+		HandleFunc(mux, `GET /api/authorized-domains`, app.listDomains).
+		HandleFunc(mux, `POST /api/authorized-domains`, app.postDomain).
+		HandleFunc(mux, `POST /api/create-signed-upload`, app.postSignedUpload).
+		Control(mux, `POST /api/donor-wall`, app.postDonorWall).
+		HandleFunc(mux, `POST /api/files-create`, app.postFileCreate).
+		HandleFunc(mux, `GET /api/files-list`, app.listFiles).
+		HandleFunc(mux, `POST /api/files-update`, app.postFileUpdate).
+		HandleFunc(mux, `GET /api/gdocs-doc`, app.getGDocsDoc).
+		HandleFunc(mux, `POST /api/gdocs-doc`, app.postGDocsDoc).
+		HandleFunc(mux, `POST /api/image-update`, app.postImageUpdate).
+		HandleFunc(mux, `GET /api/images`, app.listImages).
+		HandleFunc(mux, `POST /api/message`, app.postMessage).
+		HandleFunc(mux, `GET /api/page`, app.getPage).
+		HandleFunc(mux, `POST /api/page`, app.postPage).
+		HandleFunc(mux, `POST /api/page-create`, app.postPageCreate).
+		Control(mux, `POST /api/page-load`, app.postPageLoad).
+		HandleFunc(mux, `GET /api/pages`, app.listPages).
+		HandleFunc(mux, `GET /api/pages-by-fts`, app.listPagesByFTS).
+		HandleFunc(mux, `POST /api/page-refresh`, app.postPageRefresh).
+		HandleFunc(mux, `POST /api/shared-article`, app.postSharedArticle).
+		HandleFunc(mux, `POST /api/shared-article-from-gdocs`, app.postSharedArticleFromGDocs).
+		HandleFunc(mux, `GET /api/sidebar`, app.siteDataGet(almanack.SidebarLoc)).
+		HandleFunc(mux, `POST /api/sidebar`, app.siteDataSet(almanack.SidebarLoc)).
+		Control(mux, `GET /api/site-data`, app.getSiteData).
+		Control(mux, `POST /api/site-data`, app.postSiteData).
+		HandleFunc(mux, `GET /api/site-params`, app.siteDataGet(almanack.SiteParamsLoc)).
+		HandleFunc(mux, `POST /api/site-params`, app.siteDataSet(almanack.SiteParamsLoc))
 	// End spotlight endpoints
 
 	// Don't trust this middleware!
@@ -80,22 +83,25 @@ func (app *appEnv) routes() http.Handler {
 	// This is just a fallback.
 	ssrMW := standardMW.With(app.authCookieMiddleware)
 
-	ssrMW.Control(mux, `GET /ssr/user-info`, app.userInfo)
-	ssrMW.HandleFunc(mux, `/ssr/`, app.renderNotFound)
-	ssrMW.Control(mux, `GET /ssr/redirect/{slug}`, app.redirectSSR)
+	ssrMW.
+		Control(mux, `GET /ssr/user-info`, app.userInfo).
+		HandleFunc(mux, `/ssr/`, app.renderNotFound).
+		Control(mux, `GET /ssr/redirect/{slug}`, app.redirectSSR)
 
 	partnerSSRMW := ssrMW.With(app.hasRoleMiddleware("editor"))
 
-	partnerSSRMW.Control(mux, `GET /ssr/download-image`, app.redirectImageURL)
+	partnerSSRMW.
+		Control(mux, `GET /ssr/download-image`, app.redirectImageURL)
 
 	// Start background API endpoints
 	backgroundMW := mid.Stack{
 		httpx.WithTimeout(14 * time.Minute),
 	}
-	backgroundMW.Control(mux, `GET /api-background/cron`, app.backgroundCron)
-	backgroundMW.Control(mux, `GET /api-background/images`, app.backgroundImages)
-	backgroundMW.Control(mux, `GET /api-background/refresh-pages`, app.backgroundRefreshPages)
-	backgroundMW.Control(mux, `GET /api-background/sleep/{duration}`, app.backgroundSleep)
+	backgroundMW.
+		Control(mux, `GET /api-background/cron`, app.backgroundCron).
+		Control(mux, `GET /api-background/images`, app.backgroundImages).
+		Control(mux, `GET /api-background/refresh-pages`, app.backgroundRefreshPages).
+		Control(mux, `GET /api-background/sleep/{duration}`, app.backgroundSleep)
 	// End background API endpoints
 
 	standardMW.HandleFunc(mux, "/", app.notFound)
