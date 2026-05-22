26c6c5ead3223a5f28310b4b168ffda347bc449d
Carlana <mail@carlana.net>
2026-05-22 15:09:12 -0400

    page.sql: Simplify ListAllTopics, ListAllSeries

--- internal/db/page.sql.go
+++ internal/db/page.sql.go
@@ -134,38 +134,14 @@ func (q *Queries) GetPageByURLPath(ctx context.Context, urlPath string) (Page, e
 }
 
 const listAllSeries = `-- name: ListAllSeries :many
-WITH page_series AS (
-  SELECT
-    json_array_elements_text( --
-      coalesce((frontmatter ->> 'series'), '[]')::json) AS series,
-    publication_date
-  FROM
-    page
-),
-series_dates AS (
-  SELECT
-    series, publication_date
-  FROM
-    page_series
-  ORDER BY
-    publication_date DESC,
-    series DESC
-),
-distinct_series_dates AS (
-  SELECT DISTINCT ON (series)
-    series, publication_date
-  FROM
-    series_dates
-  ORDER BY
-    series DESC,
-    publication_date DESC
-)
 SELECT
-  series::text
+  (frontmatter ->> 'title')::text AS series
 FROM
-  distinct_series_dates
+  page
+WHERE
+  "file_path" LIKE 'content/series/%/_index.md'
 ORDER BY
-  publication_date DESC
+  "publication_date" DESC
 `
 
 func (q *Queries) ListAllSeries(ctx context.Context) ([]string, error) {
@@ -189,20 +165,14 @@ func (q *Queries) ListAllSeries(ctx context.Context) ([]string, error) {
 }
 
 const listAllTopics = `-- name: ListAllTopics :many
-WITH topics AS (
-  SELECT
-    json_array_elements_text( --
-      coalesce((frontmatter ->> 'topics'), '[]')::json) AS topic
-  FROM
-    page
-)
-SELECT DISTINCT ON (upper(topic)
-)
-  topic::text
+SELECT
+  (frontmatter ->> 'title')::text AS topic
 FROM
-  topics
+  page
+WHERE
+  "file_path" LIKE 'content/topics/%/_index.md'
 ORDER BY
-  upper(topic) ASC
+  topic ASC
 `
 
 func (q *Queries) ListAllTopics(ctx context.Context) ([]string, error) {
