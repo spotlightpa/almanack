b277daf612d0e56d61165237bab499d75fd40dbc
exe.dev user <exedev@plasma-gopher.exe.xyz>
2026-07-08 13:57:26 +0000

    Convert BulmaURLPaths to script setup style
    Co-authored-by: Shelley <shelley@exe.dev>

--- src/components/BulmaURLPaths.vue
+++ src/components/BulmaURLPaths.vue
@@ -1,55 +1,49 @@
-<script>
+<script setup>
 import BulmaAutocompleteArray from "./BulmaAutocompleteArray.vue";
 
+const props = defineProps({
+  label: String,
+  labelClass: {
+    type: String,
+    default: "label",
+  },
+  modelValue: Array,
+  options: Array,
+  placeholder: String,
+  help: String,
+  validator: Function,
+  required: {
+    type: Boolean,
+    default: false,
+  },
+  readonly: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
 function ensureLeadingSlash(value) {
-  if (!value.startsWith("/")) {
-    return "/" + value;
-  }
-  return value;
+  return value.startsWith("/") ? value : "/" + value;
 }
 
-export default {
-  components: { BulmaAutocompleteArray },
-  emits: ["update:modelValue"],
-  props: {
-    label: String,
-    labelClass: {
-      type: String,
-      default: "label",
-    },
-    modelValue: Array,
-    options: Array,
-    placeholder: String,
-    help: String,
-    validator: Function,
-    required: {
-      type: Boolean,
-      default: false,
-    },
-    readonly: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  methods: {
-    onUpdate(vals) {
-      this.$emit("update:modelValue", vals.map(ensureLeadingSlash));
-    },
-  },
-};
+function onUpdate(vals) {
+  emit("update:modelValue", vals.map(ensureLeadingSlash));
+}
 </script>
 
 <template>
   <BulmaAutocompleteArray
-    :label="label"
-    :label-class="labelClass"
-    :model-value="modelValue"
-    :options="options"
-    :placeholder="placeholder"
-    :help="help"
-    :validator="validator"
-    :required="required"
-    :readonly="readonly"
+    :label="props.label"
+    :label-class="props.labelClass"
+    :model-value="props.modelValue"
+    :options="props.options"
+    :placeholder="props.placeholder"
+    :help="props.help"
+    :validator="props.validator"
+    :required="props.required"
+    :readonly="props.readonly"
     @update:model-value="onUpdate"
   />
 </template>
