920e0fbf3bca92742dd4fbb3786a2d2b709c2334
Carlana <mail@carlana.net>
2026-08-26 09:51:00 -0400

    pgxutil: Add NilSliceToEmpty (#591)

--- internal/utils/pgxutil/null.go
+++ internal/utils/pgxutil/null.go
@@ -8,3 +8,12 @@ var (
 	NullTime = pgtype.Timestamptz{}
 	NullText = pgtype.Text{}
 )
+
+// NilSliceToEmpty turns nil slices into empty slices
+// for cases where pgx treats nil as NULL.
+func NilSliceToEmpty[S ~[]Elem, Elem any](s S) S {
+	if s == nil {
+		return make(S, 0)
+	}
+	return s
+}
